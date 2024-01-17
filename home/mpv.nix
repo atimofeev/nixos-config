b4ef052c009b6fac7083924e4c00a48c2db58aa2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-17 12:53:29 +0400

    +mpv

---
+++ home/mpv.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }: {
+  programs.mpv = {
+    enable = true;
+    config = {
+      slang = "eng,en";
+      alang = "jpn,jap,ja,jp";
+    };
+    bindings = {
+      a = "cycle audio";
+      s = "cycle sub";
+      WHEEL_UP = "add volume 2.5";
+      WHEEL_DOWN = "add volume -2.5";
+    };
+    scripts = [
+      pkgs.mpvScripts.inhibit-gnome # do not let gnome sleep during playback
+      pkgs.mpvScripts.mpris # integrate with media controls
+      pkgs.mpvScripts.autoload # load playlist entries from play dir
+      # TODO: import select-subtitle to ignore case in sub naming
+      # https://github.com/atimofeev/dotfiles/blob/main/mpv/files/scripts/select-subtitle.lua
+      # TODO: import auto-save-state to keep current playback timestamp for a specific file
+      # https://github.com/atimofeev/dotfiles/blob/main/mpv/files/scripts/auto-save-state.lua
+    ];
+  };
+}
