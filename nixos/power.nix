a15450dab9fe820abeb69e1fbf36986a0d81ed9c
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-19 13:28:44 +0400

    power: limit to max cpu freq

--- nixos/power.nix
+++ nixos/power.nix
@@ -1,14 +1,17 @@
 { ... }: {
+  # TODO: configure max cpu freq for both modes
   powerManagement.enable = true;
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
       governor = "powersave";
       turbo = "never";
+      scaling_max_freq = 1000000;
     };
     charger = {
       governor = "performance";
       turbo = "auto";
+      scaling_max_freq = 2000000;
     };
   };
 }
