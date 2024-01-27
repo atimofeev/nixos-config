7e016dff5c0bec8e253ac88bc09c7784a84fc384
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-28 00:34:07 +0400

    hyprland: more descriptive filenames

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -11,7 +11,7 @@ let
 in
 {
 
-  imports = [ ./binds.nix ./rules.nix ./settings.nix ];
+  imports = [ ./keybinds.nix ./window-rules.nix ./settings.nix ];
 
   home.packages = with pkgs; [
     sddm # display manager
