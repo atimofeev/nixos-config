f38f9097f847550f6a307f82ed3bb15837d7d440
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-01-29 00:09:14 +0400

    apps: + fun cli apps

--- nixos/apps.nix
+++ nixos/apps.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
+    # GUI/TUI apps
     firefox
     telegram-desktop
     spotify-player
     qbittorrent
+
+    # fun
+    lolcat
+    pipes
+    cmatrix
+    tmatrix
+
+    # misc
     prusa-slicer
   ];
 }
