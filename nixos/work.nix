b729749e6b216450a6d50fab2824e385085492f4
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-02-06 04:01:36 +0400

    work: pin ansible version to 2.11.6

--- nixos/work.nix
+++ nixos/work.nix
@@ -11,4 +11,17 @@
     k9s
     slack
   ];
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      ansible = pkgs.ansible.overrideAttrs (oldAttrs: {
+        version = "2.11.6";
+        src = pkgs.fetchFromGitHub {
+          owner = "ansible";
+          repo = "ansible";
+          rev = "v2.11.6";
+          sha256 = "sha256-+ljma9q1tDo0/0YQmjKO2R756BRydFgAu+2wDu+ARto=";
+        };
+      });
+    };
+  };
 }
