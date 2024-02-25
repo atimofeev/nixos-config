3b395b43b65e675db5b4485fe4ffcf6de2554f77
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-02-25 16:28:40 +0400

    boot: keep 25 latest generations

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,4 +1,7 @@
 { ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    systemd-boot.configurationLimit = 25;
+    efi.canTouchEfiVariables = true;
+  };
 }
