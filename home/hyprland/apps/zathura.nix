ce659d0cc3e89d01efe801bedafa51de509381c0
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-06 13:25:16 +0400

    zathura: catppuccin-macchiato theme

--- home/hyprland/apps/zathura.nix
+++ home/hyprland/apps/zathura.nix
@@ -1,6 +1,17 @@
-{ ... }: {
-  # example config:
-  # https://github.com/RicArch97/nixos-config/blob/main/modules/desktop/apps/zathura.nix
+{ pkgs, ... }:
+# example config:
+# https://github.com/RicArch97/nixos-config/blob/main/modules/desktop/apps/zathura.nix
+let
+  themeName = "catppuccin-macchiato";
+  themeSource = pkgs.fetchFromGitHub
+    {
+      owner = "catppuccin";
+      repo = "zathura";
+      rev = "1bda9d8274dd327b7931886ef0c5c1eb33903814";
+      sha256 = "sha256-HWOc5tnVgU/HUcVcIXACeuu3RDH1pHO/8DQRsWqumIA=";
+    } + "/src/${themeName}"; # path to theme in repo
+in
+{
   programs.zathura = {
     enable = true;
     options = {
@@ -12,5 +23,6 @@
       ";" = "focus_inputbar ':'";
       b = "adjust_window best-fit";
     };
+    extraConfig = "include ${themeSource}";
   };
 }
