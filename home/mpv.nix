7832ac72e8c5f617bbdc8780d2aedaee6d797c6e
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-07 18:37:13 +0400

    mpv: scripts new format; +TODO on simple-mpv-ui

--- home/mpv.nix
+++ home/mpv.nix
@@ -16,12 +16,17 @@
       DOWN = "add volume -2.5";
     };
 
-    scripts = [
-      pkgs.mpvScripts.inhibit-gnome # do not let gnome sleep during playback
-      pkgs.mpvScripts.mpris # integrate with media controls
-      pkgs.mpvScripts.autoload # load playlist entries from play dir
+    scripts = with pkgs.mpvScripts; [
+      inhibit-gnome # do not let gnome sleep during playback
+      mpris # integrate with media controls
+      autoload # load playlist entries from play dir
+
+      # TODO: try this out
+      # simple-mpv-ui # web ui to control mpv
+
       # TODO: import select-subtitle to ignore case in sub naming
       # https://github.com/atimofeev/dotfiles/blob/main/mpv/files/scripts/select-subtitle.lua
+
       # TODO: import auto-save-state to keep current playback timestamp for a specific file
       # https://github.com/atimofeev/dotfiles/blob/main/mpv/files/scripts/auto-save-state.lua
     ];
