d3c884ba1a65ebc2911d1276b498925539e86485
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-07 18:38:19 +0400

    git: +work config

--- home/git.nix
+++ home/git.nix
@@ -4,5 +4,12 @@
     userName = "Artem Timofeev";
     userEmail = "39891735+atimofeev@users.noreply.github.com";
     ignores = [ "*.swp" ];
+    includes = [{
+      path = "~/repos/betby/.gitconfig";
+      condition = "gitdir:~/repos/betby/";
+    }];
+    # cat ~/repos/betby/.gitconfig 
+    # [user]
+    # 	email = "181-a.timofeev@users.noreply.git.devspt.com"
   };
 }
