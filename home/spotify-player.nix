7d3d5f8461b397a7c6b25a4b0ce242d46837834a
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-03-23 15:52:38 +0400

    spotify-player: -> pkgs-unstable

--- home/spotify-player.nix
+++ home/spotify-player.nix
@@ -1,21 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs-unstable, ... }: {
   # TODO: https://github.com/catppuccin/spotify-player
-  home.packages = with pkgs; [ spotify-player ];
+  home.packages = with pkgs-unstable; [ spotify-player ];
 
-  # FIX: bump spotify-player version
-  # https://discourse.nixos.org/t/is-it-possible-to-override-cargosha256-in-buildrustpackage/4393/3
-  # nixpkgs.config = {
-  #   packageOverrides = pkgs: {
-  #     spotify-player = pkgs.spotify-player.overrideAttrs (oldAttrs: {
-  #       version = "0.17.0";
-  #       cargoHash = "sha256-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=";
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "aome510";
-  #         repo = "spotify-player";
-  #         rev = "v0.17.0";
-  #         sha256 = "sha256-fGDIlkTaRg+J6YcP9iBcJFuYm9F0UOA+v/26hhdg9/o=";
-  #       };
-  #     });
-  #   };
-  # };
 }
