332fe1692e5f99c759ac6110beac4715eb65e3fe
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-04-03 14:47:43 +0400

    home/starship: +section5: cmd_duration & status; misc cosmetics

--- home/starship.nix
+++ home/starship.nix
@@ -45,10 +45,12 @@ let
   section4 = {
     bg = colors.surface2;
     fg = colors.text;
-    fg2 = colors.yellow;
   };
-in
-{
+  section5 = {
+    bg = colors.surface0;
+    fg = colors.yellow;
+  };
+in {
   programs.starship = {
     enable = true;
     settings = {
@@ -57,15 +59,17 @@ in
       format = lib.strings.concatStrings [
         "[](${section1.bg})"
         "$battery"
-        "[](bg:${section2.bg} fg:${section1.bg})"
+        "[](fg:${section1.bg} bg:${section2.bg})"
         "$directory"
         "[](fg:${section2.bg} bg:${section3.bg})"
         "$git_branch"
         "$git_status"
         "[](fg:${section3.bg} bg:${section4.bg})"
         "$time"
+        "[](fg:${section4.bg} bg:${section5.bg})"
         "$cmd_duration"
-        "[ ](fg:${section4.bg})"
+        "$status"
+        "[ ](fg:${section5.bg})"
       ];
 
       battery = {
@@ -74,44 +78,50 @@ in
       };
 
       battery.display = [{
-        style = "bg:${section1.bg} fg:${section1.fg}";
+        style = "fg:${section1.fg} bg:${section1.bg}";
         threshold = 90;
       }];
 
       directory = {
         format = "[ $path ]($style)";
-        style = "bg:${section2.bg} fg:${section2.fg}";
+        style = "fg:${section2.fg} bg:${section2.bg}";
         truncation_symbol = "";
         truncation_length = 3;
       };
 
       git_branch = {
         format = "[ $symbol $branch ]($style)";
-        style = "bg:${section3.bg} fg:${section3.fg}";
+        style = "fg:${section3.fg} bg:${section3.bg}";
         symbol = "";
         truncation_length = 20;
       };
 
       git_status = {
         format = "[$all_status$ahead_behind ]($style)";
-        style = "bg:${section3.bg} fg:${section3.fg}";
+        style = "fg:${section3.fg} bg:${section3.bg}";
       };
 
       time = {
         disabled = false;
         format = "[ 󰧼 $time ]($style)";
-        style = "bg:${section4.bg} fg:${section4.fg}";
+        style = "fg:${section4.fg} bg:${section4.bg}";
         time_format = "%R"; # Hour:Minute Format
       };
 
       cmd_duration = {
-        format = "[ $duration ]($style)";
-        style = "bg:${section4.bg} fg:${section4.fg2}";
+        format = "[ 󱦟$duration ]($style)";
+        style = "fg:${section5.fg} bg:${section5.bg}";
         min_time = 1000;
         #Waiting for resolution of this:
         #https://github.com/starship/starship/issues/1933
         #show_notifications = true;
         #min_time_to_notify = 45000;
+      };
+
+      status = {
+        disabled = false;
+        style = "fg:${section5.fg} bg:${section5.bg}";
+        format = "[!$int]($style)";
       };
     };
   };
