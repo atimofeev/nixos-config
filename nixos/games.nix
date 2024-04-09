91ce0f61c8af05295f2a0c59dfdd24111b95fa85
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-04-09 16:30:49 +0400

    nixos/games: -razergenie & openrazer

--- nixos/games.nix
+++ nixos/games.nix
@@ -1,7 +1,5 @@
 { pkgs, vars, ... }: {
   environment.systemPackages = with pkgs; [
-    # soft
-    razergenie
     # virt
     playonlinux
     # games
@@ -12,13 +10,6 @@
     enable = true;
     # platformOptimizations.enable = true; # TODO: install https://github.com/fufexan/nix-gaming
   };
-  hardware = {
-    steam-hardware.enable = true;
 
-    openrazer = {
-      enable = true;
-      users = [ vars.username ];
-      mouseBatteryNotifier = true;
-    };
-  };
+  hardware.steam-hardware.enable = true;
 }
