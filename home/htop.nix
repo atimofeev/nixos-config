789f751f46e305398be739937ff685febd5facca
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-04-09 17:35:35 +0400

    home/htop: use htop-nvim package, + settings

--- home/htop.nix
+++ home/htop.nix
@@ -1,4 +1,13 @@
-# https://github.com/search?q=language%3Anix+programs.htop&type=code
-{ ... }: {
-  programs.htop = { enable = true; };
+{ pkgs, ... }: {
+  programs.htop = {
+    enable = true;
+    package = pkgs.htop-vim;
+    settings = {
+      hide_userland_threads = 1;
+      show_cpu_frequency = 1;
+      show_cpu_temperature = 1;
+      show_program_path = 0;
+      highlight_base_name = 1;
+    };
+  };
 }
