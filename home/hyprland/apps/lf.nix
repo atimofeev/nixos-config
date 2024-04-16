bda17eb16c0267a363eb17b76d1c941acb6a7c89
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-04-16 21:45:38 +0400

    lf: +TODOs

--- home/hyprland/apps/lf.nix
+++ home/hyprland/apps/lf.nix
@@ -1,6 +1,12 @@
 # https://mynixos.com/search?q=lf
 # https://github.com/search?q=language%3Anix+programs.lf&type=code
-# OR ranger??
+# https://github.com/BrodieRobertson/dotfiles/blob/bf3cfcb04cb23e1cd42e861506b2cf57350feaeb/config/lf/lfrc
+# https://github.com/LukeSmithxyz/voidrice/tree/master/.config/lf
+# TODO: archive and unarhive
+# single and bulk file actions: copy, cut, delete, move
+# image previews
+# filetype associations with my software
+# fzf, z.lua, d-bus, etc: https://github.com/gokcehan/lf/wiki/Integrations
 { ... }: {
   programs.lf = { enable = true; };
 }
