10e142d774e85a78cda529fa26c8c54d5f38f18c
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-04-20 23:54:55 +0400

    nixos/power: +TODO; tlp template

--- nixos/power.nix
+++ nixos/power.nix
@@ -1,5 +1,10 @@
 { ... }: {
-  powerManagement.enable = true;
+  # TODO: migrate to tlp
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+
   services = {
     power-profiles-daemon.enable = false; # to avoid collision with auto-cpufreq
     auto-cpufreq.enable = true;
@@ -18,4 +23,24 @@
       };
     };
   };
+
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #
+  #     CPU_MIN_PERF_ON_AC = 0;
+  #     CPU_MAX_PERF_ON_AC = 100;
+  #     CPU_MIN_PERF_ON_BAT = 0;
+  #     CPU_MAX_PERF_ON_BAT = 50;
+  #
+  #     START_CHARGE_THRESH_BAT0 = 60;
+  #     STOP_CHARGE_THRESH_BAT0 = 90;
+  #   };
+  # };
+
 }
