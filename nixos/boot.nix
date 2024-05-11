ae24927abc1c34e304ad17ff54a801e454debc16
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-05-11 11:52:21 +0400

    nixos/boot: -> zen kernel; keep 15 generations

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,7 +1,10 @@
-{ ... }: {
-  boot.loader = {
-    systemd-boot.enable = true;
-    systemd-boot.configurationLimit = 25;
-    efi.canTouchEfiVariables = true;
+{ pkgs, ... }: {
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    loader = {
+      systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 15;
+      efi.canTouchEfiVariables = true;
+    };
   };
 }
