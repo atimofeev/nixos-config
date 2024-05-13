51361ec66946316fa9a89293fa4435a3528b7b3b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-05-13 11:17:50 +0400

    home/gnome: +appindicator extension

--- home/gnome/extensions.nix
+++ home/gnome/extensions.nix
@@ -2,7 +2,10 @@ _: {
   dconf.settings = {
     "org/gnome/shell" = {
       disable-user-extensions = false;
-      enabled-extensions = [ "pip-on-top@rafostar.github.com" ];
+      enabled-extensions = [
+        "pip-on-top@rafostar.github.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+      ];
     };
     "org/gnome/shell/extensions/pip-on-top" = { stick = true; };
   };
