7582b54724aca6110031b4bb32e79f30cbac4803
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-07 15:30:28 +0400

    home/vcv-rack: -> stable; + vcv-config-source script

--- home/vcv-rack.nix
+++ home/vcv-rack.nix
@@ -1,11 +1,16 @@
-{ pkgs, pkgs-unstable, ... }: {
-  home.packages = with pkgs-unstable; [ vcv-rack ];
+{ pkgs, pkgs-unstable, ... }:
+let
+  vcv-config-source = pkgs.pkgs.writeShellScriptBin "source" ''
+    rm -rf $HOME/.Rack2
+    ln -s $HOME/repos/vcv-rack2-settings/ $HOME/.Rack2
+    ln -s $HOME/repos/vcv-rack2-patches/ $HOME/.Rack2/patches
+  '';
+in {
+  programs.fish.shellAliases.vcv-config-source =
+    "${vcv-config-source}/bin/source";
+  home.packages = with pkgs; [ vcv-rack ];
 
-  # TODO: source settings and patches
+  # TODO: add check for existing repos
   # git clone git@github.com:atimofeev/vcv-rack2-settings.git $HOME/repos/vcv-rack2-settings/
   # git clone git@github.com:atimofeev/vcv-rack2-patches.git $HOME/repos/vcv-rack2-patches/
-  # symlinks:
-  # SRC=DEST
-  # $HOME/repos/vcv-rack2-settings/=$HOME/.Rack2
-  # $HOME/repos/vcv-rack2-patches/=$HOME/.Rack2/patches
 }
