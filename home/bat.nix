ac09591603f6e4cf69a825d28d7eb71b9b0d66d8
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-07 22:55:30 +0400

    home/bat: fix colored tail glitches

--- home/bat.nix
+++ home/bat.nix
@@ -1,4 +1,24 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  # HACK: bat won't implement negative line range
+  # https://github.com/sharkdp/bat/issues/791
+  # https://github.com/sharkdp/bat/issues/2944
+  # Wrapper to gain tail-like functionality
+  tail = pkgs.pkgs.writeShellScriptBin "tail" ''
+    #!/usr/bin/env bash
+    file="$1"
+    show="$2"
+
+    if [ -z "$2" ]; then
+        show=10
+    fi
+
+    lines=$(wc -l < "$file")
+    range="$lines:-$((show - 1))"
+
+    exec bat "$file" --color=always --style=plain --paging=never --line-range "$range"
+  '';
+in {
   programs = {
 
     bat = {
@@ -21,6 +41,7 @@
       shellAliases = {
         less = "bat --color=always --style=auto";
         cat = "bat --color=always --style=plain --paging=never";
+        tail = "${tail}/bin/tail";
       };
       functions = {
         head = ''
@@ -33,21 +54,6 @@
           end
 
           set range "1:"$show
-
-          bat $file --color=always --style=plain --paging=never --line-range $range
-        '';
-
-        tail = ''
-          set file $argv[1]   
-
-          if set -q argv[2]
-              set show $argv[2]
-          else
-              set show 10
-          end
-
-          set lines (wc -l < $file)
-          set range (math "$lines - $show + 1")":"$lines
 
           bat $file --color=always --style=plain --paging=never --line-range $range
         '';
