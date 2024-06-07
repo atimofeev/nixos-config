2f92e6aa7fb036ff10e3c80b425a4db766ff380d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-07 13:08:06 +0400

    nixos/work/tofu: use unstable tofu

--- nixos/work/tofu.nix
+++ nixos/work/tofu.nix
@@ -1,19 +1,3 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ terraform opentofu ];
-
-  # Pin tofu version
-  # FIX: golang build fails: conflict between vendor/modules.txt and go.mod requirements
-  # nixpkgs.config = {
-  #   packageOverrides = pkgs: {
-  #     opentofu = pkgs.opentofu.overrideAttrs (oldAttrs: {
-  #       version = "1.7.1";
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "opentofu";
-  #         repo = "opentofu";
-  #         rev = "v1.7.1";
-  #         sha256 = "sha256-201zceUedEl93nyglWJo0f9SDfFX31toP0MzzHQeJds=";
-  #       };
-  #     });
-  #   };
-  # };
+{ pkgs, pkgs-unstable, ... }: {
+  environment.systemPackages = [ pkgs.terraform pkgs-unstable.opentofu ];
 }
