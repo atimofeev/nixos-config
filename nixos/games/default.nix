1601f01eaad90b1c75cd1abbd2a3275cf4d7c365
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-08 08:58:00 +0400

    nixos/games: xone -> nixos/games/xone

--- nixos/games/default.nix
+++ nixos/games/default.nix
@@ -1,4 +1,6 @@
-{ pkgs, config, vars, ... }: {
+{ pkgs, ... }: {
+  imports = [ ./xone.nix ];
+
   environment.systemPackages = with pkgs; [
     # virt
     lutris
@@ -22,58 +24,4 @@
     };
   };
 
-  hardware.xone.enable = true;
-
-  # TODO: overlays have no effect; pkgOverride fails with random error
-  # kernel patch can't find destination file to apply itself
-  # https://github.com/medusalix/xone/pull/35
-  # https://nixos.wiki/wiki/Linux_kernel#Overriding_kernel_packages
-  # https://search.nixos.org/options?channel=23.11&show=boot.kernelPatches&from=0&size=50&sort=relevance&type=packages&query=boot.kernelPatches
-  # nixpkgs.overlays = [
-  #   (final: prev: {
-  #     xone = prev.xone.overrideAttrs (oldAttrs: {
-  #       src = pkgs.fetchFromGitHub {
-  #         owner = "medusalix";
-  #         repo = "xone";
-  #         rev = "pull/35/head";
-  #         sha256 = "";
-  #       };
-  #     });
-  #   })
-  # ];
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
-  #       xone = super.linuxPackages.xone.overrideAttrs (oldAttrs: {
-  #         # xone = super.linuxPackages.xone.overrideDerivation (oldAttrs: {
-  #         version = "0.0.0";
-  #         src = pkgs.fetchFromGitHub {
-  #           owner = "medusalix";
-  #           repo = "xone";
-  #           rev = "pull/35/head";
-  #           sha256 = "";
-  #         };
-  #       });
-  #     });
-  #   })
-  # ];
-  # nixpkgs.config.packageOverrides = pkgs: rec {
-  #   linuxPackages.xone = pkgs.linuxPackages.xone.overrideDerivation (attrs: {
-  #     src = pkgs.fetchFromGitHub {
-  #       owner = "medusalix";
-  #       repo = "xone";
-  #       rev = "pull/35/head";
-  #       sha256 = "";
-  #     };
-  #   });
-  # };
-  # boot.kernelPatches = [rec {
-  #   name = "xone";
-  #   patch = pkgs.fetchpatch {
-  #     name = name + ".patch";
-  #     url =
-  #       "https://github.com/medusalix/xone/commit/954fc823fbaa429ad6e1c1a06a4a006598ef35ae.patch";
-  #     sha256 = "sha256-CS4vCSFciFJ8kH0YzNMvBwABR2wKBSzw5QIbPbAqHUs=";
-  #   };
-  # }];
 }
