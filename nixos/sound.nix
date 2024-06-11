41c56f0c6cd9b1a23fe0ce4e7509a6beac23d60f
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-11 10:29:04 +0400

    nixos/sound: still trying to fix gainy mic

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -10,11 +10,14 @@
     pulse.enable = true;
   };
 
-  # Mi Notebook: Fix +30db default gain on built-in mic
+  # FIX: Mi Notebook: Fix +30db default gain on built-in mic
+  # Replicate this: https://forum.endeavouros.com/t/microphone-gain-is-way-too-much-on-100/40575/4
+  # or this: https://github.com/0x61nas/nixfiles/blob/3ca7daa5d07716a3aa4c5113af6a5ca5263a6089/modules/workarounds/pulseaudio-mic-boost.nix
+  # or: https://github.com/ReedClanton/NixOS/blob/376707d93bb90954f9ad377b2b9a791e2e6de744/modules/nixos/sound/default.nix
   environment.etc."alsa-state.conf".source = ../assets/alsa-state.conf;
   environment.systemPackages = with pkgs; [ alsaUtils ];
   systemd.services.load-alsa-state = {
-    after = [ "sound.target" ];
+    after = [ "sound.target" "pulseaudio.service" "pipewire.service" ];
     wantedBy = [ "multi-user.target" ];
     script = ''
       ${pkgs.alsaUtils}/bin/alsactl -f /etc/alsa-state.conf restore
