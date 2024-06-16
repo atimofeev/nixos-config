6e9ce7e98091497a55195a8c6c457747d73f4a98
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-16 14:14:55 +0400

    nixos/work/tofu: +tflint with plugins

--- nixos/work/tofu.nix
+++ nixos/work/tofu.nix
@@ -1,7 +1,9 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
+{ pkgs, ... }:
+let tflint-w-plugins = pkgs.tflint.withPlugins (p: [ p.tflint-ruleset-aws ]);
+in {
+  environment.systemPackages = (with pkgs; [
     terraform
     opentofu
     tenv # https://github.com/tofuutils/tenv-nix#usage
-  ];
+  ]) ++ [ tflint-w-plugins ];
 }
