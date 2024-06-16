27d2f3682b321b1c6708dc25b39e93edcc9f0ddd
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-06-16 14:46:46 +0400

    nixos/nvidia: simplify config

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,35 +1,18 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 # steam game launch options: nvidia-offload %command%
-let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in {
-  environment.systemPackages = [ nvidia-offload pkgs.nvtopPackages.full ];
+{
+  environment.systemPackages = with pkgs; [ nvtopPackages.full gwe glxinfo ];
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     nvidiaSettings = true;
     modesetting.enable = true;
     prime = {
-      offload.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
-    };
-  };
-
-  # launch steam on dGPU
-  programs.steam = {
-    package = pkgs.steam.override {
-      extraEnv = {
-        __NV_PRIME_RENDER_OFFLOAD = 1;
-        __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-        __VK_LAYER_NV_optimus = "NVIDIA_only";
-      };
     };
   };
 }
