7edcdab50c8afe821fe69eb702c54a59ada432bd
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-07-15 13:42:48 +0400

    nixos/work/tofu: +terraformer

--- nixos/work/tofu.nix
+++ nixos/work/tofu.nix
@@ -5,5 +5,13 @@ in {
     terraform
     opentofu
     tenv # https://github.com/tofuutils/tenv-nix#usage
+    terraformer
   ]) ++ [ tflint-w-plugins ];
+
+  # NOTE: small guide on terraformer
+  # Download provider archive: https://releases.hashicorp.com/terraform-provider-aws/5.53.0/terraform-provider-aws_5.53.0_linux_amd64.zip 
+  # Unarchive and put binary into: ~/.terraform.d/plugins/linux_amd64/
+  # Launch: terraformer plan aws --resources=cloudfront --regions=eu-central-1 --profile=""
+  # cd ${resource_folder}; t init; t plan; t apply
+
 }
