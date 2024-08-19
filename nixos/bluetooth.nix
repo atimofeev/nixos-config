013169602e932d2e1c39eec7dcc35e14f68a4860
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-08-19 13:31:01 +0400

    nixos/bluetooth: BLE & handsfree struggle continues...

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,8 +1,17 @@
 _: {
-  hardware.bluetooth.settings = {
-    General = {
-      ControllerMode = "bredr"; # Fixes Marshall Motif II LE mode
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+
+      Policy.AutoEnable = true;
+
+      General = {
+        ControllerMode = "bredr"; # Fixes Marshall Motif II LE mode
+        # NOTE: trying to fix headset vs handsfree sources availability
+        # Experimental = true; 
+        # KernelExperimental = true;
+      };
+
     };
-    Policy = { AutoEnable = true; };
   };
 }
