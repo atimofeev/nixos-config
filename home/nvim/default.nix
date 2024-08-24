d601c0bcf59ee50d6ca214a3d07931474c881588
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-08-24 08:44:40 +0400

    home/nvim: disable autocmds in kitty-scrollback

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -20,17 +20,17 @@
           vim.opt.clipboard = 'unnamedplus'
         end)
 
-        vim.api.nvim_create_autocmd('TextYankPost', {
-          desc = 'Highlight when yanking (copying) text',
-          group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-          callback = function()
-            vim.highlight.on_yank({higroup='Search', timeout=300})
-          end,
-        })
 
-        -- Disable in kitty-scrollback
-        -- https://github.com/mikesmithgh/kitty-scrollback.nvim/issues/265
+        -- Disabled in kitty-scrollback
         if vim.env.KITTY_SCROLLBACK_NVIM ~= 'true' then
+          vim.api.nvim_create_autocmd('TextYankPost', {
+            desc = 'Highlight when yanking (copying) text',
+            group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+            callback = function()
+              vim.highlight.on_yank({higroup='Search', timeout=300})
+            end,
+          })
+
           vim.api.nvim_create_autocmd("TermClose", {
             desc = 'Close terminal if its process has ended',
             callback = function()
