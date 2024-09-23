c4acf576e948d17e14e1e2b7f738034fecf43bc5
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-09-24 00:29:23 +0400

    home/git: rewrite

---
+++ home/git.nix
@@ -0,0 +1,18 @@
+{ vars, ... }: {
+  programs.git = {
+    enable = true;
+    userName = "Artem Timofeev";
+    userEmail = "39891735+atimofeev@users.noreply.github.com";
+    ignores = [ "*.swp" ".null-ls_*" ];
+    extraConfig = { pull.rebase = true; };
+    includes = [{
+      path = "~/repos/betby/.gitconfig";
+      condition = "gitdir:~/repos/betby/";
+    }];
+  };
+
+  sops.secrets."work/gitlab-config" = {
+    path = "/home/${vars.username}/repos/betby/.gitconfig";
+  };
+
+}
