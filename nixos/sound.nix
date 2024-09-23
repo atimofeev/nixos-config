583a3bcc9bed61aae5a1c3a93d3d86792e62969d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-09-24 00:25:39 +0400

    nixos/sound: still trying to fix int mic gain

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -1,13 +1,84 @@
-{ pkgs, ... }: {
+{ pkgs, lib, vars, ... }:
+let
+  # Patch out microphone boost. Done when boost defaults to on and breaks microphone.
+  # See: https://community.frame.work/t/microphone-extremely-staticy/15533/12
+  pipewire-no-mic-boost = let volumeLevel = 50;
+  in pkgs.pipewire.overrideAttrs (og-pkg: {
+    name = "${og-pkg.pname}-no-mic-boost";
+    buildCommand = ''
+      set -euo pipefail
+
+      # Copy original files, for each split-output (`out`, `dev` etc.).
+      # TODO: Remove hard coded refrence to pipewire so this function will
+      #   work with any package.
+      ${lib.concatStringsSep "\n" (map (outputName: ''
+        echo "Copying output ${outputName}"
+        set -x
+        cp -a ${pkgs.pipewire.${outputName}} ''$${outputName}
+        set +x
+      '') og-pkg.outputs)}
+
+      set -x
+
+      INFILE1=$out/share/alsa-card-profile/mixer/paths/analog-input-internal-mic.conf
+      INFILE2=$out/share/alsa-card-profile/mixer/paths/analog-input-internal-mic-always.conf
+
+      cat $INFILE1 | \
+        ${pkgs.python3}/bin/python -c \
+          'import re,sys; print(re.sub("\[Element Capture\]\nswitch = mute\nvolume = merge", "[Element Capture]\nswitch = mute\nvolume = ${
+            toString volumeLevel
+          }", sys.stdin.read()))' | \
+           ${pkgs.python3}/bin/python -c \
+          'import re,sys; print(re.sub("\[Element Internal Mic Boost\]\nrequired-any = any\nswitch = select\nvolume = merge", "[Element Internal Mic Boost]\nrequired-any = any\nswitch = select\nvolume = 0", sys.stdin.read()))' | \
+          ${pkgs.python3}/bin/python -c \
+          'import re,sys; print(re.sub("\[Element Int Mic Boost\]\nrequired-any = any\nswitch = select\nvolume = merge", "[Element Int Mic Boost]\nrequired-any = any\nswitch = select\nvolume = 0", sys.stdin.read()))' > \
+          tmp.conf
+
+      # Ensure file changed (something was replaced)
+      ! cmp tmp.conf $INFILE1
+      chmod +w $INFILE1
+      cp tmp.conf $INFILE1
+
+      cat $INFILE2 | \
+        ${pkgs.python3}/bin/python -c \
+        'import re,sys; print(re.sub("\[Element Capture\]\nswitch = mute\nvolume = merge", "[Element Capture]\nswitch = mute\nvolume = ${
+          toString volumeLevel
+        }", sys.stdin.read()))' | \
+         ${pkgs.python3}/bin/python -c \
+        'import re,sys; print(re.sub("\[Element Internal Mic Boost\]\nswitch = select\nvolume = merge", "[Element Internal Mic Boost]\nswitch = select\nvolume = 0", sys.stdin.read()))' | \
+        ${pkgs.python3}/bin/python -c \
+        'import re,sys; print(re.sub("\[Element Int Mic Boost\]\nswitch = select\nvolume = merge", "[Element Int Mic Boost]\nswitch = select\nvolume = 0", sys.stdin.read()))' > \
+        tmp.conf
+
+      # Ensure file changed (something was replaced)
+      ! cmp tmp.conf $INFILE2
+      chmod +w $INFILE2
+      cp tmp.conf $INFILE2
+
+      set +x
+    '';
+  });
+in {
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
+  users.users.${vars.username}.extraGroups = [ "audio" ];
+
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    package = pipewire-no-mic-boost;
+    audio.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
+    jack.enable = true;
+
+    extraConfig.pipewire = {
+      "10-logging" = { "context.properties"."log.level" = 3; };
+    };
   };
 
   # FIX: Mi Notebook: Fix +30db default gain on built-in mic
@@ -16,29 +87,7 @@
   # or: https://github.com/ReedClanton/NixOS/blob/376707d93bb90954f9ad377b2b9a791e2e6de744/modules/nixos/sound/default.nix
   # https://github.com/little-dude/home/blob/570c32312c93f2359e2b62e3fcea8432c1fec4ee/nix/os/renzo.nix#L107
   # https://github.com/kittywitch/arcnmx-home/blob/f40988631ca8473f5207bc966dae10ef015eb4e0/nodes/shanghai/audio.nix#L346
-  # https://mynixos.com/nixpkgs/option/services.pipewire.wireplumber.extraConfig
-  # https://wiki.archlinux.org/title/PipeWire
-  # https://wiki.archlinux.org/title/WirePlumber
+  # https://github.com/NixOS/nixpkgs/issues/24184
+  # https://unix.stackexchange.com/questions/336790/how-to-prevent-white-noise-in-headphones-on-dell-xps-13-9350-9360/581386#581386
 
-  # maybe also see through all of these:
-  # https://github.com/search?q=%22Internal+Mic+Boost%22&type=code&p=1
-  # https://github.com/search?q=language%3Anix+mic+boost+alsa&type=code
-
-  # there's also an option in analog-input-internal-mic.conf:
-  # [Option Internal Mic Boost:off]
-  # name = input-boost-off
-
-  environment.etc."alsa-state.conf".source = ../assets/alsa-state.conf;
-  environment.systemPackages = with pkgs; [ alsaUtils ];
-  systemd.services.load-alsa-state = {
-    after = [ "sound.target" "pulseaudio.service" "pipewire.service" ];
-    wantedBy = [ "multi-user.target" ];
-    script = ''
-      ${pkgs.alsaUtils}/bin/alsactl -f /etc/alsa-state.conf restore
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-  };
 }
