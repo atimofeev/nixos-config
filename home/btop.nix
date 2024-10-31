bfa47213dca61181b171ee7f539fca44382c6d88
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-10-31 14:07:38 +0100

    home: +btop

---
+++ home/btop.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+  # NOTE: configure: https://github.com/aristocratos/btop#configurability
+
+  programs.btop = {
+    enable = true;
+    settings = { color_theme = "catppuccin_macchiato"; };
+  };
+
+  xdg.configFile."btop/themes/catppuccin_macchiato.theme".source =
+    pkgs.fetchurl {
+      url =
+        "https://raw.githubusercontent.com/catppuccin/btop/f437574b600f1c6d932627050b15ff5153b58fa3/themes/catppuccin_macchiato.theme";
+      hash = "sha256-+LGMyyF71OvBhIBqkdSaEssxK5zzfYuiMyJlOnisiFA=";
+    };
+
+}
--- home/default.nix
+++ home/default.nix
@@ -7,6 +7,7 @@
     ./rofi
 
     ./bat.nix
+    ./btop.nix
     ./firefox.nix
     ./fish.nix
     ./git.nix
