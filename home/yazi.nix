ac0e22ce97486f9f5edc431bf9fda95168a63694
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-10-31 13:39:47 +0100

    home/yazi: + catppuccin theme

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,1 +1,17 @@
-_: { programs.yazi.enable = true; }
+{ pkgs, ... }:
+let
+  themeName = "catppuccin-macchiato-blue.toml";
+
+  themeSource = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "yazi";
+    rev = "54d868433a0c2f3e1651114136ea088eef72a4a7";
+    sha256 = "sha256-dMXSXS3Scj1LZZqqnvvC37VWSyjSQZg9thvjcm2iNSM=";
+  } + "/themes/macchiato/${themeName}"; # path to theme in repo
+
+in {
+
+  programs.yazi.enable = true;
+
+  xdg.configFile."yazi/theme.toml".source = themeSource;
+}
