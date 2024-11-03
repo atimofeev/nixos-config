b5c473552fabdf319a7ac48c1bf6da4a4983c77a
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-04 00:48:52 +0100

    home/k9s/plugins/kubeconfig: update

--- home/k9s/plugins/kubeconfig.nix
+++ home/k9s/plugins/kubeconfig.nix
@@ -10,30 +10,35 @@
       args = [
         "-c"
         ''
-          >-
           server=$(kubectl config view --minify --output 'jsonpath={.clusters[0].cluster.server}')
           ca=$(kubectl get secret/$NAME-secret -n $NAMESPACE --context $CONTEXT -o jsonpath='{.data.ca\.crt}')
           token=$(kubectl get secret/$NAME-secret -n $NAMESPACE --context $CONTEXT -o jsonpath='{.data.token}' | base64 --decode)
+          ns="default"
 
-          echo "
+          echo "---
           apiVersion: v1
           kind: Config
-          clusters:
-          - name: $CONTEXT
-            cluster:
-              server: $server
-              certificate-authority-data: $ca
-          users:
-          - name: $NAME
-            user:
-              token: $token
-          contexts:
-          - name: $CONTEXT-$NAME
-            context:
-              user: $NAME
-              cluster: $CONTEXT
-              namespace: $NAMESPACE
+
           current-context: $CONTEXT
+
+          contexts:
+            - name: $CONTEXT-$NAME
+              context:
+                user: $NAME
+                cluster: $CONTEXT
+                namespace: $ns
+
+          clusters:
+            - name: $CONTEXT
+              cluster:
+                server: $server
+                certificate-authority-data: $ca
+
+          users:
+            - name: $NAME
+              user:
+                token: $token
+
           " > ${config.home.homeDirectory}/$NAME_$CONTEXT.yaml
         ''
       ];
