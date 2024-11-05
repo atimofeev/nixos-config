6560271c5d4ad670181b8c809719db6d54b6f032
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-05 16:20:16 +0100

    home/hyprland: update window-rules

--- home/hyprland/window-rules.nix
+++ home/hyprland/window-rules.nix
@@ -20,22 +20,19 @@
     windowrulev2 = [
       "float, class:steam, title:^(Steam Settings|Sign in to Steam)$"
       "float, class:firefox, title:^(About Mozilla Firefox)$"
-
       "float, title:^(.*Huddle.*)$"
-      "idleinhibit, title:^(.*Huddle.*)$"
-      "idleinhibit, title:^(Meet.*)$"
-      "idleinhibit, title:^(.*Miro)$"
 
-      # FIX: opaque windows on special workspace
-      "opacity 1.0 1.0, onworkspace:s[true]"
+      "opacity 1.0 override, onworkspace:s[true]"
 
       "workspace special, class:^(spotify_player)$"
       "workspace special, class:^(org.telegram.desktop)$"
-      "fullscreen, class:^(org.telegram.desktop)$ title:^(Media viever)$"
+      # FIX: telegram media is half-broken in special
+      # "workspace 1, class:^(org.telegram.desktop)$ title:^(Media viever)$"
+      # "fullscreen, class:^(org.telegram.desktop)$ title:^(Media viever)$"
       # "workspace unset, class:^(org.telegram.desktop)$ title:^(Media viever)$"
       # "float, class:^(org.telegram.desktop)$ title:^(Media viever)$"
 
-      "opacity 0.9 0.6, class:^([Rr]ofi)$"
+      # "opacity 0.9 0.6, class:^([Rr]ofi)$"
       # "opacity 0.9 0.7, class:^(firefox)$"
       # "opacity 0.9 0.8, class:^([Tt]hunar)$"
       # "opacity 0.8 0.6, class:^(pcmanfm-qt)$"
@@ -46,7 +43,7 @@
 
       # Picture-in-a-Picture (PIP) rules: Oddly, some need re-duplication.  This is because the window for
       # PIP changes after on first launch, and will not inherit the rules...
-      "opacity 1 1, title:^(Firefox|Picture-in-Picture)$"
+      "opacity 1.0 override, title:^(Firefox|Picture-in-Picture)$"
       "pin, title:^(Firefox|Picture-in-Picture)$"
       "float, title:^(Firefox|Picture-in-Picture)$"
       "size 25% 25%, title:^(Firefox|Picture-in-Picture)$"
