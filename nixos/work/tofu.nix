af1188a5b7b0d548e378ea66dd6e0acef62eaa40
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-07 10:34:17 +0100

    nixos/work/tofu: remove secrets

--- nixos/work/tofu.nix
+++ nixos/work/tofu.nix
@@ -1,7 +1,7 @@
 { pkgs, config, vars, ... }:
 let tflint-w-plugins = pkgs.tflint.withPlugins (p: [ p.tflint-ruleset-aws ]);
 in {
-  environment.systemPackages = (with pkgs; [
+  environment.systemPackages = [ tflint-w-plugins ] ++ (with pkgs; [
     opentofu
 
     # import tools
@@ -11,7 +11,7 @@ in {
     # misc
     tenv # https://github.com/tofuutils/tenv-nix#usage
     tftui
-  ]) ++ [ tflint-w-plugins ];
+  ]);
 
   # NOTE: small guide on terraformer
   # Download provider archive: https://releases.hashicorp.com/terraform-provider-aws/5.53.0/terraform-provider-aws_5.53.0_linux_amd64.zip 
@@ -23,8 +23,6 @@ in {
     "work/env/TF_HTTP_PASSWORD".owner = vars.username;
     "work/env/AWS_ACCESS_KEY_ID".owner = vars.username;
     "work/env/AWS_SECRET_ACCESS_KEY".owner = vars.username;
-    "work/env/GAMBITE_AWS_ACCESS_KEY_ID".owner = vars.username;
-    "work/env/GAMBITE_AWS_SECRET_ACCESS_KEY".owner = vars.username;
     "work/env/CLOUDFLARE_API_TOKEN".owner = vars.username;
     "work/env/GITLAB_TOKEN".owner = vars.username;
   };
@@ -38,12 +36,6 @@ in {
     })"
     export AWS_SECRET_ACCESS_KEY="$(cat ${
       config.sops.secrets."work/env/AWS_SECRET_ACCESS_KEY".path
-    })"
-    export TF_VAR_gambite_access_key="$(cat ${
-      config.sops.secrets."work/env/GAMBITE_AWS_ACCESS_KEY_ID".path
-    })"
-    export TF_VAR_gambite_secret_key="$(cat ${
-      config.sops.secrets."work/env/GAMBITE_AWS_SECRET_ACCESS_KEY".path
     })"
     export CLOUDFLARE_API_TOKEN="$(cat ${
       config.sops.secrets."work/env/CLOUDFLARE_API_TOKEN".path
