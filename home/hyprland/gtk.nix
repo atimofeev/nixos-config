93202cc69e5b0f25b8b6a0ed03da650ebf823566
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-12 00:23:11 +0100

    home/hyprland/gtk: fix iconTheme; remove obsolete gtk config

--- home/hyprland/gtk.nix
+++ home/hyprland/gtk.nix
@@ -4,8 +4,6 @@ in {
 
   gtk = {
     enable = true;
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
-    gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
     theme = {
       package = f pkgs.gnome.gnome-themes-extra;
       name = "Adwaita-dark";
@@ -13,7 +11,7 @@ in {
 
     iconTheme = {
       package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita-dark";
+      name = "Adwaita";
     };
 
     # font = {
