f3045c0706ac34a032e511ad6bd56044e3cd9388
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-12 00:28:50 +0100

    nixos/desktop: refactor - Remove gnome DE & apps - Enable xwayland - Fix xdg-desktop-portal packages & config

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,32 +1,39 @@
-{ inputs, pkgs, vars, ... }: {
+{ inputs, pkgs, lib, vars, ... }: {
 
   environment = {
 
     # use wayland for electron apps & chromium
-    sessionVariables.NIXOS_OZONE_WL = "1";
+    # sessionVariables.NIXOS_OZONE_WL = "1";
 
-    systemPackages = with pkgs; [
-      switcheroo
-      gnome-graphs
-      gnome.gnome-tweaks
-      gnome.dconf-editor
-      gnomeExtensions.pop-shell
-      gnomeExtensions.appindicator
-      unstable.letterpress
-      unstable.gnomeExtensions.pip-on-top # update to v8 for compatibility with Gnome 46
-      unstable.gnomeExtensions.gamemode-shell-extension
-    ];
+    systemPackages = with pkgs;
+      [
+        # switcheroo
+        # gnome-graphs
+        # gnome.gnome-tweaks
+        # gnome.dconf-editor
+        # gnomeExtensions.pop-shell
+        # gnomeExtensions.appindicator
+        # unstable.letterpress
+        # unstable.gnomeExtensions.pip-on-top # update to v8 for compatibility with Gnome 46
+        # unstable.gnomeExtensions.gamemode-shell-extension
+      ];
 
   };
 
-  services.xserver = {
-    enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
-    xkb = {
-      layout = vars.kb_layouts;
-      options = "grp:win_space_toggle";
+  services = {
+    gvfs.enable = true;
+    upower.enable = true;
+
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      # desktopManager.gnome.enable = true;
+      xkb = {
+        layout = vars.kb_layouts;
+        options = "grp:win_space_toggle";
+      };
     };
+
   };
 
   programs = {
@@ -39,21 +46,31 @@
     hyprland = {
       enable = true;
       package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-      #xwayland.enable = true;
+      portalPackage =
+        inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+      xwayland.enable = true;
     };
 
   };
 
   xdg.portal = {
     enable = true;
-    configPackages = [
+    configPackages = lib.mkDefault
+      [ inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland ];
+    extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
     ];
-    # extraPortals = [
-    #   inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
-    #   pkgs.xdg-desktop-portal-gtk
-    # ];
+    config = {
+      common.default = [ "hyprland" ];
+      hyprland.default = [ "gtk" "hyprland" ];
+    };
+  };
+
+  security.pam.services.hyprlock = {
+    text = ''
+      auth include login
+    '';
   };
 
 }
