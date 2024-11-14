7bb43870ae4d822014d7b6064b4b2ee2be100be4
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-14 10:32:13 +0100

    home/hyprland: update window-rules

--- home/hyprland/window-rules.nix
+++ home/hyprland/window-rules.nix
@@ -1,17 +1,11 @@
-{ vars, ... }: {
+_: {
   wayland.windowManager.hyprland.settings = {
 
     windowrule = [
-      "float, nm-connection-editor|blueman-manager"
-      "float, nwg-look|qt5ct|mpv"
-      "float, eog"
-      "float, zoom"
-      "float, rofi"
-      "float, gnome-system-monitor"
-      "float, yad"
-      "center, ^(pavucontrol) "
-
       "float, pavucontrol|pwvucontrol"
+      "float, nm-connection-editor|blueman-manager"
+      "float, mpv"
+      "float, zoom"
       "float, nvidia-settings"
     ];
 
@@ -23,22 +17,13 @@
       "opacity 1.0 override, onworkspace:s[true]"
 
       "workspace 10, class:^(Slack)$"
-      "workspace special, class:^(spotify_player)$"
-      "workspace special, class:^(org.telegram.desktop)$"
+      "workspace special silent, class:^(spotify_player)$"
+      "workspace special silent, class:^(org.telegram.desktop)$"
       # FIX: telegram media is half-broken in special
       # "workspace 1, class:^(org.telegram.desktop)$ title:^(Media viever)$"
       # "fullscreen, class:^(org.telegram.desktop)$ title:^(Media viever)$"
       # "workspace unset, class:^(org.telegram.desktop)$ title:^(Media viever)$"
       # "float, class:^(org.telegram.desktop)$ title:^(Media viever)$"
-
-      # "opacity 0.9 0.6, class:^([Rr]ofi)$"
-      # "opacity 0.9 0.7, class:^(firefox)$"
-      # "opacity 0.9 0.8, class:^([Tt]hunar)$"
-      # "opacity 0.8 0.6, class:^(pcmanfm-qt)$"
-      # "opacity 0.9 0.7, class:^(gedit)$"
-      # "opacity 0.95 0.8, class:^(${vars.terminal.name})$"
-      # "opacity 0.9 0.7, class:^(mousepad)$"
-      # "opacity 0.9 0.7, class:^(yad)$"
 
       # Picture-in-a-Picture (PIP) rules: Oddly, some need re-duplication.  This is because the window for
       # PIP changes after on first launch, and will not inherit the rules...
