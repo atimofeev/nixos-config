91fc5ecdaf4f8cdde0c5ada991674a584a350357
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-17 12:55:06 +0100

    home/hyprland/hyprpaper: disable splash

--- home/hyprland/hyprpaper.nix
+++ home/hyprland/hyprpaper.nix
@@ -4,6 +4,7 @@ _: {
     enable = true;
     settings = {
       ipc = "off";
+      splash = false;
       preload = [ "${../../assets/dark-shore.png}" ];
       wallpaper = [ ",${../../assets/dark-shore.png}" ];
     };
