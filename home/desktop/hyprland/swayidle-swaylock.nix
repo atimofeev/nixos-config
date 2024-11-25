428524ac047880c1cd2b0ee4d3bd043293745438
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-11-25 10:52:25 +0100

    home/desktop/hyprland/swayidle-swaylock: +idleinhibit for fullscreen YT

--- home/desktop/hyprland/swayidle-swaylock.nix
+++ home/desktop/hyprland/swayidle-swaylock.nix
@@ -15,6 +15,7 @@ in {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "idleinhibit focus, fullscreen:1"
+      "idleinhibit always, fullscreen:1 class:^(firefox)$ title:^(.*YouTube.*)$"
       "idleinhibit focus, class:^(firefox)$ title:^(.*Miro.*)$"
       "idleinhibit always, class:^(Slack)$ title:^(.*Huddle.*)$"
       "idleinhibit always, class:^(firefox)$ title:^(Meet.*)$"
