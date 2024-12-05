96292bb68531d3223a5b2ecdeb69cc8edaed5d35
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-12-05 11:17:42 +0100

    nixos: show diff on config activation

--- nixos/default.nix
+++ nixos/default.nix
@@ -29,6 +29,13 @@
   ];
 
   system.stateVersion = vars.nix.stateVersion;
+  system = {
+    activationScripts.diff = ''
+      if [[ -e /run/current-system ]]; then
+        ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+      fi
+    '';
+  };
 
   nix = {
     settings = {
