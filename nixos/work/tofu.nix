2ebb6760dc244949ad41dc67948cb719685674b0
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2024-12-13 18:16:51 +0100

    nixos/work/tofu: remove terraformer NOTE

--- nixos/work/tofu.nix
+++ nixos/work/tofu.nix
@@ -13,12 +13,6 @@ in {
     tftui
   ]);
 
-  # NOTE: small guide on terraformer
-  # Download provider archive: https://releases.hashicorp.com/terraform-provider-aws/5.53.0/terraform-provider-aws_5.53.0_linux_amd64.zip 
-  # Unarchive and put binary into: ~/.terraform.d/plugins/linux_amd64/
-  # Launch: terraformer plan aws --resources=cloudfront --regions=eu-central-1 --profile=""
-  # cd ${resource_folder}; t init; t plan; t apply
-
   sops.secrets = {
     "work/env/TF_HTTP_PASSWORD".owner = vars.username;
     "work/env/AWS_ACCESS_KEY_ID".owner = vars.username;
