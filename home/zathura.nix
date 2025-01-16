f708c2fbf23ea8b0f44377978cbcc9e863590ff3
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-01-16 18:14:14 +0100

    zathura: new keymaps

--- home/zathura.nix
+++ home/zathura.nix
@@ -3,15 +3,13 @@
 # https://github.com/RicArch97/nixos-config/blob/main/modules/desktop/apps/zathura.nix
 let
   themeName = "catppuccin-macchiato";
-  themeSource = pkgs.fetchFromGitHub
-    {
-      owner = "catppuccin";
-      repo = "zathura";
-      rev = "1bda9d8274dd327b7931886ef0c5c1eb33903814";
-      sha256 = "sha256-HWOc5tnVgU/HUcVcIXACeuu3RDH1pHO/8DQRsWqumIA=";
-    } + "/src/${themeName}"; # path to theme in repo
-in
-{
+  themeSource = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "zathura";
+    rev = "1bda9d8274dd327b7931886ef0c5c1eb33903814";
+    sha256 = "sha256-HWOc5tnVgU/HUcVcIXACeuu3RDH1pHO/8DQRsWqumIA=";
+  } + "/src/${themeName}"; # path to theme in repo
+in {
   programs.zathura = {
     enable = true;
     options = {
@@ -22,6 +20,8 @@ in
     mappings = {
       ";" = "focus_inputbar ':'";
       b = "adjust_window best-fit";
+      w = "adjust_window width";
+      t = "recolor";
     };
     extraConfig = "include ${themeSource}";
   };
