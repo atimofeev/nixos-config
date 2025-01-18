251523b9b6b5af3781a52d918f9276ef1f5c1177
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-01-18 11:22:02 +0100

    hacks: add note

--- hosts/milaptop/hacks.nix
+++ hosts/milaptop/hacks.nix
@@ -4,6 +4,7 @@
 
     # NOTE: fix handsfree on Marshall Motiff II
     # NOTE: fix hyprpanel sound OCD after pipewire restart
+    # NOTE: fix wayland-pipewire-idle-inhibit after pipewire restart
     programs.fish.shellAliases.pipewire-restart = ''
       systemctl --user restart pipewire.service && \
       systemctl --user restart pipewire-pulse.service && \
