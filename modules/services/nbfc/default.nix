521707d8c76319eac4239b9007ff925fafd564ae
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-01-20 00:49:00 +0100

    nbfc: improve config; still breaks bluetooth

--- modules/services/nbfc/default.nix
+++ modules/services/nbfc/default.nix
@@ -1,34 +1,30 @@
-{ pkgs, vars, ... }:
-# NOTE: https://github.com/nbfc-linux/nbfc-linux/blob/main/nixos-installation-new.md
+{ pkgs, ... }:
 let
+
+  nbfc = pkgs.nbfc-linux;
 
   configName = "Xiaomi Mi Book (TM1613, TM1703)";
 
-  command =
-    "bin/nbfc_service --config-file '/home/${vars.username}/.config/nbfc.json'";
+  cfg = pkgs.writeText "nbfc.json" (builtins.toJSON {
+    SelectedConfigId = "${configName}";
+    EmbeddedControllerType = "ec_sys_linux";
+  });
 
 in {
 
-  home-manager.users.${vars.username} = {
-    xdg.configFile = {
-      "nbfc.json" = {
-        text = # json
-          ''
-            {"SelectedConfigId": "${configName}"}
-          '';
-
-      };
-    };
-  };
-
-  environment.systemPackages = with pkgs; [ unstable.nbfc-linux ];
-  systemd.services.nbfc_service = {
+  environment.systemPackages = [ nbfc ];
+  systemd.services.nbfc = {
     enable = true;
     description = "NoteBook FanControl service";
-    serviceConfig.Type = "simple";
     path = [ pkgs.kmod ];
     wantedBy = [ "multi-user.target" ];
-    script = "${pkgs.unstable.nbfc-linux}/${command}";
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "3";
+      ExecStart = "${nbfc}/bin/nbfc_service -c ${cfg}";
+      TimeoutStopSec = "5";
+    };
   };
 
 }
