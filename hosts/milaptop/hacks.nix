cb1414189637cacdca938e823bb9e14ffb487a82
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-01-27 15:33:08 +0100

    milaptop host: update restart-pipewire hack

--- hosts/milaptop/hacks.nix
+++ hosts/milaptop/hacks.nix
@@ -2,14 +2,17 @@
 
   home-manager.users.${vars.username} = {
 
+    # FIX: there is clearly something very wrong with this approach
     # NOTE: fix handsfree on Marshall Motiff II
-    # NOTE: fix hyprpanel sound OCD after pipewire restart
-    # NOTE: fix wayland-pipewire-idle-inhibit after pipewire restart
+    # fix hyprpanel sound OCD after pipewire restart
+    # fix wayland-pipewire-idle-inhibit after pipewire restart
+    # fix xdg-desktop-portal after pipewire restart
     programs.fish.shellAliases.pipewire-restart = ''
       systemctl --user restart pipewire.service && \
       systemctl --user restart pipewire-pulse.service && \
       systemctl --user restart hyprpanel && \
-      systemctl --user restart wayland-pipewire-idle-inhibit.service
+      systemctl --user restart wayland-pipewire-idle-inhibit.service && \
+      systemctl --user restart xdg-desktop-portal
     '';
 
   };
