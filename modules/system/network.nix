7068e26e933648ca2ff4270d55c0986be00ce2e5
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-02-27 16:24:23 +0100

    network: use systemd-resolved for dns

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -2,9 +2,22 @@
 
   networking = {
     hostName = hostname;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+    };
   };
 
   users.users.${vars.username}.extraGroups = [ "networkmanager" ];
+
+  sercives.resolved = {
+    enable = true;
+    dnssec = "allow-downgrade";
+    fallbackDns =
+      [ "1.1.1.1" "2606:4700:4700::1111" "8.8.8.8" "2001:4860:4860::8844" ];
+    extraConfig = ''
+      DNSStubListener=no
+    '';
+  };
 
 }
