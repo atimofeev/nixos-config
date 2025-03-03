7e0afb5b3b9be1162d94546da6aadba46e733633
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-03 18:44:42 +0100

    firefox: disable DNSOverHTTPS

--- home/firefox.nix
+++ home/firefox.nix
@@ -6,6 +6,10 @@
       # NOTE: Can't be enabled outside US and some other countries
       # AutofillAddressEnabled = true;
       # AutofillCreditCardEnabled = true;
+      DNSOverHTTPS = {
+        Enabled = false;
+        Locked = true;
+      };
       DisableAppUpdate = true;
       DisableFirefoxScreenshots = true;
       DisableFirefoxStudies = true;
