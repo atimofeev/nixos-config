a380e03c8eaeb3086d4c43958ee122f97ac42342
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-03 22:44:37 +0100

    home-manager: remove redundant input libx

--- modules/system/home-manager.nix
+++ modules/system/home-manager.nix
@@ -1,9 +1,9 @@
-{ inputs, pkgs, libx, vars, ... }: {
+{ inputs, pkgs, vars, ... }: {
 
   imports = [ inputs.home-manager.nixosModules.default ];
 
   home-manager = {
-    extraSpecialArgs = { inherit inputs pkgs libx vars; };
+    extraSpecialArgs = { inherit inputs pkgs vars; };
     users.${vars.username} = import ../../home;
   };
 
