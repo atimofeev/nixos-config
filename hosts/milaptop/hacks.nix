5f61fb190070c6eef178cf297069dfa0e71350bc
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-05 09:59:40 +0100

    milaptop: implement better hack for stuck handsfree on Motiff II

--- hosts/milaptop/hacks.nix
+++ hosts/milaptop/hacks.nix
@@ -2,17 +2,12 @@
 
   home-manager.users.${vars.username} = {
 
-    # FIX: there is clearly something very wrong with this approach
-    # NOTE: fix handsfree on Marshall Motiff II
-    # fix hyprpanel sound OCD after pipewire restart
-    # fix wayland-pipewire-idle-inhibit after pipewire restart
-    # fix xdg-desktop-portal after pipewire restart
-    programs.fish.shellAliases.pipewire-restart = ''
-      systemctl --user restart pipewire.service && \
-      systemctl --user restart pipewire-pulse.service && \
-      systemctl --user restart hyprpanel && \
-      systemctl --user restart wayland-pipewire-idle-inhibit.service && \
-      systemctl --user restart xdg-desktop-portal'';
+    # NOTE: fix stuck handsfree on Marshall Motiff II
+    programs.fish.shellAliases.reconnect-motiff2 = ''
+      bluetoothctl disconnect 00:25:D1:37:8C:19 && \
+      sleep 3 && \
+      bluetoothctl connect 00:25:D1:37:8C:19'';
 
   };
+
 }
