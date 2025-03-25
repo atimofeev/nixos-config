7bd91cad55c641769f2e95df915944dffa605de6
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-03-25 16:36:16 +0100

    kubecolor: add TODO

--- home/terminal/apps/kubecolor.nix
+++ home/terminal/apps/kubecolor.nix
@@ -1,5 +1,9 @@
 _: {
 
+  # TODO: improve colorscheme (or even port catppuccin?)
+  # https://kubecolor.github.io/customizing/themes/
+  # https://kubecolor.github.io/reference/config/
+
   programs.kubecolor = {
     enable = true;
     enableAlias = true;
