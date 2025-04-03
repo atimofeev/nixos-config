1a6f3819ae5d0900c368636bf120c25aac30c367
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-03 11:58:43 +0200

    hyprland: remove ags config stub

--- modules/desktop/hyprland/config/apps/ags/default.nix
+++
@@ -1,4 +0,0 @@
-# NOTE: example https://github.com/0thElement/nixconf/blob/0b83b3dde83de16f2bb6ae474390b21978461e9e/packages/ags/default.nix#L4
-  # TODO:
-  # ags config on volume/brighness change
-  # https://github.com/A7R7/hypr-config/blob/64da4c050740798bc890552f4fbb48cd4f2d7a30/hyprland.org?plain=1#L303
