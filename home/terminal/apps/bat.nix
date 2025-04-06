4335dae91d3e010277326efc977202f42d6e9c51
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-06 15:53:44 +0200

    bat: moved fish aliases to commonAliases; used them for all shells

--- home/terminal/apps/bat.nix
+++ home/terminal/apps/bat.nix
@@ -1,9 +1,5 @@
-{ pkgs, ... }:
+{ lib, pkgs, config, ... }:
 let
-  # HACK: bat won't implement negative line range
-  # https://github.com/sharkdp/bat/issues/791
-  # https://github.com/sharkdp/bat/issues/2944
-  # Wrapper to gain tail-like functionality
   tail = pkgs.pkgs.writeShellScriptBin "tail" ''
     #!/usr/bin/env bash
     file="$1"
@@ -16,8 +12,25 @@ let
     lines=$(wc -l < "$file")
     range="$lines:-$((show - 1))"
 
-    exec bat "$file" --color=always --style=plain --paging=never --line-range "$range"
-  '';
+    exec bat "$file" --color=always --style=plain --paging=never --line-range "$range"'';
+
+  head = pkgs.pkgs.writeShellScriptBin "head" ''
+    #!/usr/bin/env bash
+    file="$1"
+    show="$2"
+
+    if [ -z "$2" ]; then
+        show=10
+    fi
+
+    exec bat "$file" --color=always --style=plain --paging=never --line-range 1:"$show"'';
+
+  commonAliases = {
+    less = "bat --color=always --style=auto";
+    cat = "bat --color=always --style=plain --paging=never";
+    tail = "${tail}/bin/tail";
+    head = "${head}/bin/head";
+  };
 in {
   programs = {
 
@@ -37,20 +50,13 @@ in {
       };
     };
 
-    fish = {
-      shellAliases = {
-        less = "bat --color=always --style=auto";
-        cat = "bat --color=always --style=plain --paging=never";
-        tail = "${tail}/bin/tail";
-      };
-      functions = {
-        head = # fish
-          ''
-            set file $argv[1]
-            set show (set -q argv[2]; and echo $argv[2]; or echo 10)
-            bat $file --color=always --style=plain --paging=never --line-range 1:$show'';
-      };
-    };
+    nushell =
+      lib.mkIf config.programs.nushell.enable { shellAliases = commonAliases; };
+    fish =
+      lib.mkIf config.programs.fish.enable { shellAliases = commonAliases; };
+    bash =
+      lib.mkIf config.programs.bash.enable { shellAliases = commonAliases; };
+    zsh = lib.mkIf config.programs.zsh.enable { shellAliases = commonAliases; };
   };
 
 }
