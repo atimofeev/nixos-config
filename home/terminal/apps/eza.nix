e97da575bb271d455d368ffe6b94ece935417d73
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-06 16:10:49 +0200

    eza: sourced catppuccin theme

--- home/terminal/apps/eza.nix
+++ home/terminal/apps/eza.nix
@@ -1,5 +1,14 @@
-{ lib, config, ... }:
+{ lib,pkgs, config, ... }:
 let
+  themeName = "catppuccin.yml";
+
+  themeSource = pkgs.fetchFromGitHub {
+    owner = "eza-community";
+    repo = "eza-themes";
+    rev = "57149851f07b3ee6ca94f5fe3d9d552f73f8b8b4";
+    sha256 = "sha256-vu6QLz0RvPavpD2VED25D2PJlHgQ8Yis+DnL+BPlvHw=";
+  } + "/themes/${themeName}"; # path to theme in repo
+
   commonAliases = {
     ls = "eza";
     ll = "eza --long";
@@ -29,4 +38,6 @@ in {
     zsh = lib.mkIf config.programs.zsh.enable { shellAliases = commonAliases; };
   };
 
+  xdg.configFile."eza/theme.yml".source = themeSource;
+  
 }
