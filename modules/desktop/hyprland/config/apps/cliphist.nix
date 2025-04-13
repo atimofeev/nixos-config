0b1cc6661fef6b9b68744776c4edbf540de99545
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-13 19:53:16 +0200

    cliphist: configured 'paste-history via fuzzel' hotkey

--- modules/desktop/hyprland/config/apps/cliphist.nix
+++ modules/desktop/hyprland/config/apps/cliphist.nix
@@ -1,10 +1,16 @@
-{ lib, ... }: {
+{ lib, pkgs, ... }: {
 
-  # NOTE: usage:
-  # fzf: cliphist list | fzf --no-sort | cliphist decode | wl-copy
-  # fuzzel: cliphist list | fuzzel --dmenu | cliphist decode | wl-copy
+  wayland.windowManager.hyprland.settings.bind = let
+    cliphist = "${pkgs.cliphist}/bin/cliphist";
+    fuzzel = "${pkgs.fuzzel}/bin/fuzzel";
+    wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+    wtype = "${pkgs.wtype}/bin/wtype";
+  in [
+    "SUPER, V, exec, ${cliphist} list | ${fuzzel} --dmenu | ${cliphist} decode | ${wl-copy} && ${wtype} -M ctrl -P v -m ctrl -p v"
+  ];
 
-  # NOTE: https://github.com/nix-community/home-manager/pull/6751
+  # NOTE: remove during upgrade to 25.05
+  # https://github.com/nix-community/home-manager/pull/6751
   systemd.user.services.cliphist.Unit.After =
     lib.mkForce "graphical-session.target";
   systemd.user.services.cliphist-images.Unit.After =
@@ -13,6 +19,7 @@
   services.cliphist = {
     enable = true;
     allowImages = true;
+    # NOTE: switch during upgrade to 25.05
     # systemdTargets = [ "graphical-session.target" ];
     systemdTarget = "graphical-session.target";
   };
