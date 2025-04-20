a41877ea531a9136133ff4bdc1e3e9220a129858
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-21 00:35:04 +0200

    network: added sysctl settings to bypass hotspot restrictions Increased TTL & hop limit

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -1,5 +1,13 @@
 { vars, ... }: {
 
+  # Bypass hotspot restrictions for certain ISPs
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_default_ttl" = 65;
+    "net.ipv6.conf.all.hop_limit" = 65;
+    "net.ipv6.conf.default.hop_limit" = 65;
+    "net.ipv6.conf.*.hop_limit" = 65;
+  };
+
   networking.networkmanager = {
     enable = true;
     dns = "systemd-resolved";
