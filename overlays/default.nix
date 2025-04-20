52a19af36c81ca0a6045999e88d58b87af6731c7
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-20 12:07:51 +0200

    overlays: added xow_dongle-firmware override Trying to fix dongle pairing button

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,6 +5,7 @@
       (import ./unstable.nix { inherit inputs; })
       (import ./terraformer.nix)
       (import ./neovim-unwrapped.nix)
+      (import ./xow_dongle-firmware.nix)
 
       # (import ../overlays/xone.nix { inherit pkgs; })
 
