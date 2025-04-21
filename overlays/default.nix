068d2ccf4cceb8be4c8b13a96b7ce61db364cd65
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-21 19:39:55 +0200

    overlays: finally fixed xone overlay Trying to fix dongle pairing button

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,13 +2,11 @@
 
   nixpkgs = {
     overlays = [
-      (import ./unstable.nix { inherit inputs; })
-      (import ./terraformer.nix)
       (import ./neovim-unwrapped.nix)
+      (import ./terraformer.nix)
+      (import ./unstable.nix { inherit inputs; })
+      (import ./xone.nix)
       (import ./xow_dongle-firmware.nix)
-
-      # (import ../overlays/xone.nix { inherit pkgs; })
-
     ];
   };
 
--- overlays/xone.nix
+++ overlays/xone.nix
@@ -1,41 +1,14 @@
-# https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/xone/default.nix
-# https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/hardware/xone.nix
-# https://github.com/medusalix/xone/pull/45
-{ pkgs, ... }:
-
-# NOTE: no effect
-
-# final: prev: {
-#   xone = prev.xone.overrideAttrs (oldAttrs: {
-#     src = pkgs.fetchFromGitHub {
-#       owner = "medusalix";
-#       repo = "xone";
-#       # rev = "refs/pull/45/head";
-#       rev = "3463d0e2335998c1b94bb1605dc7a6668d4ba262";
-#       sha256 = "";
-#     };
-#   });
-# }
-
-# NOTE: no effect
-
-# final: prev: {
-#   linuxPackages = prev.linuxPackages.extend (lpself: lpsuper: {
-#     # xone = lpsuper.xone.overrideAttrs (oldAttrs: {
-#     xone = prev.xone.overrideAttrs (oldAttrs: {
-#       src = pkgs.fetchFromGitHub {
-#         owner = "medusalix";
-#         repo = "xone";
-#         rev = "3463d0e2335998c1b94bb1605dc7a6668d4ba262";
-#         sha256 = "";
-#       };
-#     });
-#   });
-# }
-
-# NOTE: no effect
-
-# final: prev: {
-#   linuxPackages = prev.linuxPackages.extend
-#     (lpfinal: lpprev: { xone = lpfinal.callPackage ../pkgs/xone.nix; });
-# }
+final: prev: {
+  linuxPackages_latest = prev.linuxPackages_latest.extend (lpfinal: lpprev: {
+    xone = lpprev.xone.overrideAttrs (oldAttrs: {
+      version = "0.3-unstable-2024-12-23";
+      src = prev.fetchFromGitHub {
+        owner = "dlundqvist";
+        repo = "xone";
+        rev = "c682b0cd4fd56d2d9639b64787034a375535eb4b";
+        sha256 = "sha256-QGmrOCiMa/Nrm2ln7aO+QVL3F5nAK2n6qWMVn+VMwcM=";
+      };
+      patches = [ ];
+    });
+  });
+}
