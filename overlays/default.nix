d7e7d8b11b5a58ed06b0bbb765d45a594e6545ab
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-22 16:12:58 +0200

    overlays: formatted

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,11 @@
 { inputs, ... }: {
 
-  nixpkgs = {
-    overlays = [
-      (import ./neovim-unwrapped.nix)
-      (import ./terraformer.nix)
-      (import ./unstable.nix { inherit inputs; })
-      (import ./xone.nix)
-      (import ./xow_dongle-firmware-045e_02e6.nix)
-    ];
-  };
+  nixpkgs.overlays = [
+    (import ./neovim-unwrapped.nix)
+    (import ./terraformer.nix)
+    (import ./unstable.nix { inherit inputs; })
+    (import ./xone.nix)
+    (import ./xow_dongle-firmware-045e_02e6.nix)
+  ];
 
 }
