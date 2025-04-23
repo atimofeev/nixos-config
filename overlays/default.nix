54c8276eb62118663d7cd99c8850a432513a9478
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-04-23 17:41:49 +0200

    overlays: added note regarding upgrade to 25.05 release

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,8 +4,11 @@
     (import ./neovim-unwrapped.nix)
     (import ./terraformer.nix)
     (import ./unstable.nix { inherit inputs; })
+
+    # NOTE: remove during upgrade to 25.05(?)
     (import ./xone.nix)
     (import ./xow_dongle-pr-400938.nix)
+
   ];
 
 }
