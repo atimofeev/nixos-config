c9e2a1c505a5cf01bf63f7acebf5d2142a8a97ad
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-05 15:46:19 +0200

    network: simplified resolved config

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -8,16 +8,13 @@
     "net.ipv6.conf.*.hop_limit" = 65;
   };
 
+  users.users.${vars.username}.extraGroups = [ "networkmanager" ];
+
   networking.networkmanager = {
     enable = true;
     dns = "systemd-resolved";
   };
 
-  users.users.${vars.username}.extraGroups = [ "networkmanager" ];
-
-  services.resolved = {
-    enable = true;
-    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
-  };
+  services.resolved.enable = true;
 
 }
