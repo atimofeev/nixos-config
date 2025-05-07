e8d37650b476823a077d7391850536f88e5a09cc
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-07 20:33:31 +0200

    Revert "cliphist: improved keybind command to fail with no fuzzel selection"
    This reverts commit d3e7c03a2a13cd33b53eed053d12d6ae9d5a8ad6.

--- modules/desktop/hyprland/config/apps/cliphist.nix
+++ modules/desktop/hyprland/config/apps/cliphist.nix
@@ -6,8 +6,7 @@
     wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
     wtype = "${pkgs.wtype}/bin/wtype";
   in [
-    # NOTE: properly exit if fuzzel returns nothing
-    "SUPER, V, exec, selected_item=$(${cliphist} list | ${fuzzel} --dmenu) && [ -n $selected_item ] && echo $selected_item | ${cliphist} decode | ${wl-copy} && ${wtype} -M ctrl -P v -m ctrl -p v"
+    "SUPER, V, exec, ${cliphist} list | ${fuzzel} --dmenu | ${cliphist} decode | ${wl-copy} && ${wtype} -M ctrl -P v -m ctrl -p v"
   ];
 
   # NOTE: remove during upgrade to 25.05
