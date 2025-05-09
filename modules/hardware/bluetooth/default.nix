82901e83eb768509d0ec885a6ddca81a4fd8e8d2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-09 10:49:32 +0200

    feat(bluetooth): enabled blueman and blueman-applet Added bluetuith package

--- modules/hardware/bluetooth/default.nix
+++ modules/hardware/bluetooth/default.nix
@@ -1,4 +1,10 @@
-_: {
+{ pkgs, vars, ... }: {
+
+  environment.systemPackages = [ pkgs.bluetuith ];
+
+  services.blueman.enable = true;
+
+  home-manager.users.${vars.username}.services.blueman-applet.enable = true;
 
   hardware.bluetooth = {
     enable = true;
