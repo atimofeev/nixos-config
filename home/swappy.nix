cbec302a2599467fe76f0d2107ae105ca8d5f114
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-11 19:57:46 +0200

    chore(swappy): added a todo milestone

--- home/swappy.nix
+++ home/swappy.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }: {
+
+  # TODO: create home-manager module
+  # example: https://github.com/nix-community/home-manager/pull/3904
   home.packages = with pkgs; [ swappy ];
 
   xdg.configFile."swappy/config".text = ''
