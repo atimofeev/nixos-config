e2eeeb2c07f3587da3d8a4197604fa2fb45eefbc
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-05-18 20:57:29 +0200

    feat(rofi): added uswm prefix; reformatted let variables

--- modules/desktop/hyprland/config/apps/rofi.nix
+++ modules/desktop/hyprland/config/apps/rofi.nix
@@ -1,11 +1,19 @@
-{ pkgs, vars, ... }:
+{ pkgs, vars, osConfig, ... }:
 let
+
   themeSource = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "rofi";
     rev = "c24a212a6b07c2d45f32d01d7f10b4d88ddc9f45";
     sha256 = "sha256-WGYEA4Q7UvSaRDjP/DiEtfXjvmWbewtdyJWRpjhbZgg=";
   };
+
+  uwsm = "${pkgs.uwsm}/bin/uwsm";
+  prefix = if osConfig.programs.hyprland.withUWSM then "${uwsm} app --" else "";
+  pkill = "${pkgs.procps}/bin/pkill";
+  wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
+  wtype = "${pkgs.wtype}/bin/wtype";
+
 in {
 
   xdg.configFile."rofi/catppuccin-mocha.rasi".source =
@@ -27,15 +35,12 @@ in {
       display-emoji = "󰞅 ";
       hover-select = true;
       show-icons = true;
+      run-command = "${prefix} {cmd}";
     };
 
   };
 
-  wayland.windowManager.hyprland.settings.bind = let
-    pkill = "${pkgs.procps}/bin/pkill";
-    wl-copy = "${pkgs.wl-clipboard}/bin/wl-copy";
-    wtype = "${pkgs.wtype}/bin/wtype";
-  in [''
+  wayland.windowManager.hyprland.settings.bind = [''
     SUPER, A, exec, ${pkill} rofi || rofi -show drun -no-history -calc-command "echo -n '{result}' | ${wl-copy} && ${wtype} -M ctrl -P v -m ctrl -p v"
   ''];
 
