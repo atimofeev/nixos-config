a67d60bfea8e0fce596f500c41b8c43fb96549d0
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-06-03 19:56:19 +0200

    feat(git): enabled diff-so-fancy; added a lot of config options

--- home/terminal/apps/git.nix
+++ home/terminal/apps/git.nix
@@ -1,29 +1,99 @@
 { config, ... }:
 {
+
+  sops.secrets."work/gitlab-config" = {
+    path = "${config.home.homeDirectory}/repos/betby/.gitconfig";
+  };
+
   programs.git = {
     enable = true;
     userName = "Artem Timofeev";
     userEmail = "39891735+atimofeev@users.noreply.github.com";
+    diff-so-fancy = {
+      enable = true;
+      markEmptyLines = false;
+    };
     ignores = [
       "*.swp"
       ".null-ls_*"
       ".terraform*"
       "terraform.tfstate*"
     ];
-    extraConfig = {
-      init.defaultBranch = "main";
-      pull.rebase = true;
-    };
     includes = [
       {
         path = "~/repos/betby/.gitconfig";
         condition = "gitdir:~/repos/betby/";
       }
     ];
-  };
-
-  sops.secrets."work/gitlab-config" = {
-    path = "${config.home.homeDirectory}/repos/betby/.gitconfig";
+    extraConfig = {
+      # commit.gpgSign = true;
+      # user.signingkey = "123456";
+      advice = {
+        addEmptyPathspec = false;
+        pushNonFastForward = false;
+        statusHints = false;
+      };
+      branch = {
+        sort = "-committerdate";
+      };
+      color = {
+        decorate = {
+          HEAD = "red";
+          branch = "blue";
+          remoteBranch = "magenta";
+          tag = "yellow";
+        };
+        branch = {
+          current = "magenta";
+          local = "default";
+          plain = "blue";
+          remote = "yellow";
+          upstream = "green";
+        };
+      };
+      core = {
+        compression = 9;
+        preloadindex = true;
+        whitespace = "error";
+      };
+      diff = {
+        context = 3;
+        interHunkContext = 10;
+        renames = "copies";
+      };
+      init.defaultBranch = "main";
+      interactive = {
+        singlekey = true;
+      };
+      log = {
+        abbrevCommit = true;
+        graphColors = "blue,yellow,cyan,magenta,green,red";
+      };
+      pager = {
+        branch = false;
+        tag = false;
+      };
+      pull = {
+        default = "current";
+        rebase = true;
+      };
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+        followTags = true;
+      };
+      status = {
+        branch = true;
+        showStash = true;
+        showUntackedFiles = "all";
+      };
+      tag = {
+        sort = "-taggerdate";
+      };
+      url = {
+        "git@github.com:".insteadOf = "gh:";
+      };
+    };
   };
 
 }
