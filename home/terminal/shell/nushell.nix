0cab196a281f6fa9540ab3186babf43d6374b88a
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-06-12 11:02:29 +0200

    feat(nushell): configured fuzzy completion

--- home/terminal/shell/nushell.nix
+++ home/terminal/shell/nushell.nix
@@ -22,10 +22,12 @@
           $env.config = {
             show_banner: false,
             render_right_prompt_on_last_line: false,
-            # NOTE: no fuzzy completion https://github.com/nushell/nushell/issues/1275
-            # completion_mode: "fuzzy",
-            # completion_mode: "list",
-            # completion_algorithm: fuzzy,
+            completions: {
+              case_sensitive: false
+              quick: true
+              partial: true
+              algorithm: "fuzzy"
+            }           
           }
 
           plugin add ${pkgs.nushellPlugins.query}/bin/nu_plugin_query
