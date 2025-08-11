b0def7678226ff2d40bb3e012ac229421cda1a24
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-08-11 12:55:05 +0200

    feat(kanata): simplify config for a faster tap-hold behavior

--- modules_v2/services/kanata.nix
+++ modules_v2/services/kanata.nix
@@ -26,60 +26,25 @@ in
               )
 
               (defvar
-
-                tap-time 250 ;; double-hit key within this timeout to repeat tap action
-                hold-time 175
-
-                ;; TODO: complete per-finger timout config
-                little-hold-time 175
-                ring-hold-time 165
-                middle-hold-time 160
-                index-hold-time 150
-
-                left-hand-keys (
-                  q w e r t
-                  a s d f g
-                  z x c v b
-                )
-
-                right-hand-keys (
-                  y u i o p
-                  h j k l ;
-                  n m , . /
-                )
-
-              )
-
-              (deffakekeys
-                to-base (layer-switch base)   
-              )
-
-              (defalias
-                tap (multi
-                  (layer-switch nomods)
-                  (on-idle-fakekey to-base tap 20)
-                )
+                tap-time 200 ;; double-hit key within this timeout to repeat tap action
+                hold-time 200
               )
 
               (defalias
                 escnav (tap-hold $tap-time $hold-time esc (layer-while-held nav))
-                a (tap-hold-release-keys $tap-time $hold-time (multi a @tap) lmet $left-hand-keys)
-                s (tap-hold-release-keys $tap-time $hold-time (multi s @tap) lalt $left-hand-keys)
-                d (tap-hold-release-keys $tap-time $hold-time (multi d @tap) lsft $left-hand-keys)
-                f (tap-hold-release-keys $tap-time $hold-time (multi f @tap) lctl $left-hand-keys)
+                a (tap-hold $tap-time $hold-time a lmet)
+                s (tap-hold $tap-time $hold-time s lalt)
+                d (tap-hold $tap-time $hold-time d lsft)
+                f (tap-hold $tap-time $hold-time f lctl)
                 h _
-                j (tap-hold-release-keys $tap-time $hold-time (multi j @tap) rctl $right-hand-keys)
-                k (tap-hold-release-keys $tap-time $hold-time (multi k @tap) rsft $right-hand-keys)
-                l (tap-hold-release-keys $tap-time $hold-time (multi l @tap) ralt $right-hand-keys)
-                ; (tap-hold-release-keys $tap-time $hold-time (multi ; @tap) rmet $right-hand-keys)
+                j (tap-hold $tap-time $hold-time j rctl)
+                k (tap-hold $tap-time $hold-time k rsft)
+                l (tap-hold $tap-time $hold-time l ralt)
+                sc (tap-hold $tap-time $hold-time ; rmet)
               )
 
               (deflayer base
-                @escnav @a @s @d @f @h @j @k @l @;
-              )
-
-              (deflayer nomods
-                @escnav a s d f h j k l ;
+                @escnav @a @s @d @f @h @j @k @l @sc
               )
 
               (deflayer nav
