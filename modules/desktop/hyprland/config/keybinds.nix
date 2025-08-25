de61ee4e3ba0abe1050c1972ae1c08ab4e35decd
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-08-25 19:17:05 +0200

    feat(hyprland/keybinds): updated bracketleft/bracketright behavior will utilize only current monitor

--- modules/desktop/hyprland/config/keybinds.nix
+++ modules/desktop/hyprland/config/keybinds.nix
@@ -114,8 +114,8 @@ in
       # workspaces
       "SUPER, page_up, workspace, m-1"
       "SUPER, page_down, workspace, m+1"
-      "SUPER, bracketleft, workspace, -1"
-      "SUPER, bracketright, workspace, +1"
+      "SUPER, bracketleft, workspace, r-1"
+      "SUPER, bracketright, workspace, r+1"
       "SUPER, mouse_up, workspace, m+1"
       "SUPER, mouse_down, workspace, m-1"
       "SUPER SHIFT, U, movetoworkspace, special"
