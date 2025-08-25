17fd8d8923c28ae84acae8bfa1181225ae54cc36
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-08-25 19:04:46 +0200

    chore(hyprland/keybinds): formatted

--- modules/desktop/hyprland/config/keybinds.nix
+++ modules/desktop/hyprland/config/keybinds.nix
@@ -58,92 +58,91 @@ in
       ", xf86MonBrightnessUp, exec, ${brightnessctl} set 5%+ -q"
     ];
 
-    bind =
-      [
-        # apps
-        "SUPER, Return, exec, ${term}"
-        "SUPER SHIFT, Return, exec, ${term} -e ${editor}"
-        "SUPER, E, exec, ${term} -e yazi"
-        "SUPER SHIFT, H, exec, ${term} -e btop"
-        "SUPER SHIFT, N, exec, ${term} -e nvtop"
-        "SUPER SHIFT, S, exec, ${term} -o term=xterm-kitty --class spotify_player -e spotify_player"
-        "SUPER SHIFT, B, exec, ${prefix} firefox --new-window"
+    bind = [
+      # apps
+      "SUPER, Return, exec, ${term}"
+      "SUPER SHIFT, Return, exec, ${term} -e ${editor}"
+      "SUPER, E, exec, ${term} -e yazi"
+      "SUPER SHIFT, H, exec, ${term} -e btop"
+      "SUPER SHIFT, N, exec, ${term} -e nvtop"
+      "SUPER SHIFT, S, exec, ${term} -o term=xterm-kitty --class spotify_player -e spotify_player"
+      "SUPER SHIFT, B, exec, ${prefix} firefox --new-window"
 
-        # Make screenshots!
-        ", Print, exec, ${hyprshot} -m region --clipboard-only --freeze"
-        "ALT, Print, exec, ${hyprshot} -m window --clipboard-only --freeze"
-        "SHIFT, Print, exec, ${hyprshot} -m output --clipboard-only --freeze"
-        ", xf86Cut, exec, ${hyprshot} -m region --raw --freeze | ${swappy} -f -" # region -> edit
-        "SUPER, Print, exec, ${wl-paste} | ${swappy} -f -" # clipboard -> edit
+      # Make screenshots!
+      ", Print, exec, ${hyprshot} -m region --clipboard-only --freeze"
+      "ALT, Print, exec, ${hyprshot} -m window --clipboard-only --freeze"
+      "SHIFT, Print, exec, ${hyprshot} -m output --clipboard-only --freeze"
+      ", xf86Cut, exec, ${hyprshot} -m region --raw --freeze | ${swappy} -f -" # region -> edit
+      "SUPER, Print, exec, ${wl-paste} | ${swappy} -f -" # clipboard -> edit
 
-        # Record screen!
-        # wf-recorder
-        # wf-recorder -g "$(slurp)"
-        # wf-recorder --audio
-        # wf-recorder -f "name.mp4"
+      # Record screen!
+      # wf-recorder
+      # wf-recorder -g "$(slurp)"
+      # wf-recorder --audio
+      # wf-recorder -f "name.mp4"
 
-        # main
-        "SUPER, Q, killactive" # or closewindow?
-        "SUPER, F, fullscreen"
-        "SUPER SHIFT, F, togglefloating"
-        "SUPER, P, pseudo" # dwindle layout
-        "SUPER, S, togglesplit" # dwindle layout
-        "SUPER SHIFT, L, exec, ${loginctl} lock-session"
+      # main
+      "SUPER, Q, killactive" # or closewindow?
+      "SUPER, F, fullscreen"
+      "SUPER SHIFT, F, togglefloating"
+      "SUPER, P, pseudo" # dwindle layout
+      "SUPER, S, togglesplit" # dwindle layout
+      "SUPER SHIFT, L, exec, ${loginctl} lock-session"
 
-        # group
-        "SUPER, G, togglegroup"
-        "ALT, tab, changegroupactive"
-        "SUPER, tab, changegroupactive"
+      # group
+      "SUPER, G, togglegroup"
+      "ALT, tab, changegroupactive"
+      "SUPER, tab, changegroupactive"
 
-        # move focus
-        "SUPER, h, movefocus, l"
-        "SUPER, j, movefocus, d"
-        "SUPER, k, movefocus, u"
-        "SUPER, l, movefocus, r"
+      # move focus
+      "SUPER, h, movefocus, l"
+      "SUPER, j, movefocus, d"
+      "SUPER, k, movefocus, u"
+      "SUPER, l, movefocus, r"
 
-        # move windows
-        "SUPER CTRL, h, movewindow, l"
-        "SUPER CTRL, j, movewindow, d"
-        "SUPER CTRL, k, movewindow, u"
-        "SUPER CTRL, l, movewindow, r"
+      # move windows
+      "SUPER CTRL, h, movewindow, l"
+      "SUPER CTRL, j, movewindow, d"
+      "SUPER CTRL, k, movewindow, u"
+      "SUPER CTRL, l, movewindow, r"
 
-        # workspaces
-        "SUPER, page_up, workspace, m-1"
-        "SUPER, page_down, workspace, m+1"
-        "SUPER, bracketleft, workspace, -1"
-        "SUPER, bracketright, workspace, +1"
-        "SUPER, mouse_up, workspace, m+1"
-        "SUPER, mouse_down, workspace, m-1"
-        "SUPER SHIFT, U, movetoworkspace, special"
-        "SUPER, U, togglespecialworkspace,"
-        "SUPER CTRL, page_up, movetoworkspace, -1"
-        "SUPER CTRL, page_down, movetoworkspace, +1"
-        "SUPER SHIFT, page_up, movetoworkspacesilent, -1"
-        "SUPER SHIFT, page_down, movetoworkspacesilent, +1"
-        "SUPER CTRL, bracketleft, movetoworkspace, -1"
-        "SUPER CTRL, bracketright, movetoworkspace, +1"
-        "SUPER SHIFT, bracketleft, movetoworkspacesilent, -1"
-        "SUPER SHIFT, bracketright, movetoworkspacesilent, +1"
+      # workspaces
+      "SUPER, page_up, workspace, m-1"
+      "SUPER, page_down, workspace, m+1"
+      "SUPER, bracketleft, workspace, -1"
+      "SUPER, bracketright, workspace, +1"
+      "SUPER, mouse_up, workspace, m+1"
+      "SUPER, mouse_down, workspace, m-1"
+      "SUPER SHIFT, U, movetoworkspace, special"
+      "SUPER, U, togglespecialworkspace,"
+      "SUPER CTRL, page_up, movetoworkspace, -1"
+      "SUPER CTRL, page_down, movetoworkspace, +1"
+      "SUPER SHIFT, page_up, movetoworkspacesilent, -1"
+      "SUPER SHIFT, page_down, movetoworkspacesilent, +1"
+      "SUPER CTRL, bracketleft, movetoworkspace, -1"
+      "SUPER CTRL, bracketright, movetoworkspace, +1"
+      "SUPER SHIFT, bracketleft, movetoworkspacesilent, -1"
+      "SUPER SHIFT, bracketright, movetoworkspacesilent, +1"
 
-        # 1..10 workspaces
-      ]
-      ++ (builtins.concatLists (
-        builtins.genList (
-          x:
-          let
-            ws =
-              let
-                c = builtins.div (x + 1) 10;
-              in
-              builtins.toString (x + 1 - (c * 10));
-          in
-          [
-            "SUPER, ${ws}, workspace, ${toString (x + 1)}"
-            "SUPER CTRL, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            "SUPER SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
-          ]
-        ) 10
-      ));
+      # 1..10 workspaces
+    ]
+    ++ (builtins.concatLists (
+      builtins.genList (
+        x:
+        let
+          ws =
+            let
+              c = builtins.div (x + 1) 10;
+            in
+            builtins.toString (x + 1 - (c * 10));
+        in
+        [
+          "SUPER, ${ws}, workspace, ${toString (x + 1)}"
+          "SUPER CTRL, ${ws}, movetoworkspace, ${toString (x + 1)}"
+          "SUPER SHIFT, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
+        ]
+      ) 10
+    ));
 
   };
 }
