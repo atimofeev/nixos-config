874abcc45325463867ac2a61add48c71041c03eb
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-07 14:49:33 +0200

    chore(logind): removed invalid unused options

--- modules/system/logind.nix
+++ modules/system/logind.nix
@@ -6,14 +6,6 @@ in
 
   options.custom.system.logind = {
     enable = lib.mkEnableOption "logind bundle";
-    battery_performance = lib.mkOption {
-      default = "power";
-      type = lib.types.str;
-    };
-    charger_performance = lib.mkOption {
-      default = "performance";
-      type = lib.types.str;
-    };
   };
 
   config = lib.mkIf cfg.enable {
