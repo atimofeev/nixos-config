a32b49cb84619252231516668ccb6354a56c5d11
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-08 10:45:11 +0200

    chore(flake): bumped all inputs; pinned linux to 6.17; applied nvidia fix

--- home/nix-index.nix
+++ home/nix-index.nix
@@ -1,10 +1,10 @@
 { inputs, ... }:
 {
 
-  imports = [ inputs.nix-index-database.hmModules.nix-index ];
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
 
   programs = {
-    # nix-index.enable = true; # show packages for missing command (command-not-found)
+    nix-index.enable = false; # show packages for missing command (command-not-found)
     nix-index-database.comma.enable = true;
   };
 
