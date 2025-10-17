69b436bb89d9cde03d60622bf17c2ba9cab0e1eb
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-17 22:03:43 +0200

    chore(host/zefir): removed non-functional kernelParams

--- hosts/zefir/default.nix
+++ hosts/zefir/default.nix
@@ -24,14 +24,6 @@
   networking.hostName = "zefir";
   time.timeZone = "Europe/Podgorica";
 
-  # FIX: broken
-  # These flags are used to enable backlight control when the dGPU is working in hybrid mode
-  boot.kernelParams = [
-    "i915.enable_dpcd_backlight=1"
-    "nvidia.NVreg_EnableBacklightHandler=0"
-    "nvidia.NVReg_RegistryDwords=EnableBrightnessControl=0"
-  ];
-
   hardware = {
     intelgpu.driver = "xe";
     enableRedistributableFirmware = true;
