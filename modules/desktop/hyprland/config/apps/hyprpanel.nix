a083610780516d735d740bad6967bc7cf629b5ba
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-20 13:04:19 +0200

    feat(hyprpanel): disabled autoHide

--- modules/desktop/hyprland/config/apps/hyprpanel.nix
+++ modules/desktop/hyprland/config/apps/hyprpanel.nix
@@ -30,7 +30,7 @@ in
     settings = {
 
       bar = {
-        autoHide = "single-window";
+        # autoHide = "single-window";
         bluetooth = {
           label = true;
           rightClick = blueman-manager;
