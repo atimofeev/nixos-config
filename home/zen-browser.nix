f80e34e082103249e0a72dd3ad85bef67ec914e9
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-22 00:45:39 +0200

    feat(zen-browser): added more settings, inherit from firefox

--- home/zen-browser.nix
+++ home/zen-browser.nix
@@ -16,7 +16,27 @@
     inherit (config.programs.firefox) policies;
     profiles.default = {
       isDefault = true;
-      inherit (config.programs.firefox.profiles.default) settings;
+      inherit (config.programs.firefox.profiles.default) userContent;
+      settings = config.programs.firefox.profiles.default.settings // {
+        # "zen.workspaces.natural-scroll" = true;
+        # "zen.view.compact.hide-tabbar" = true;
+        # "zen.view.compact.animate-sidebar" = false;
+        "zen.tabs.show-newtab-vertical" = false;
+        "zen.theme.accent-color" = "#8aadf4";
+        "zen.urlbar.behavior" = "float";
+        "zen.view.compact.enable-at-startup" = true;
+        "zen.view.compact.hide-toolbar" = true;
+        "zen.view.compact.toolbar-flash-popup" = true;
+        "zen.view.show-newtab-button-top" = false;
+        "zen.view.window.scheme" = 0;
+        "zen.welcome-screen.seen" = true;
+        "zen.workspaces.continue-where-left-off" = true;
+      };
+      search = {
+        force = true;
+        default = "google";
+        inherit (config.programs.firefox.profiles.default.search) engines;
+      };
     };
   };
 
