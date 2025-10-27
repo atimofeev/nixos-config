07c3fc66f5a3b6e50603ed0f5ca8a1dd2068852c
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-10-27 21:41:31 +0100

    feat(home/catppuccin): enabled globally

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -4,29 +4,18 @@
   imports = [ inputs.catppuccin.homeModules.catppuccin ];
 
   catppuccin = {
+    enable = true;
     inherit (osConfig.catppuccin) accent flavor;
+
     # NOTE: wait for 25.11
     # eza.enable = true;
-    atuin.enable = true;
-    bat.enable = true;
-    btop.enable = true;
-    cursors.enable = false; # I use adwaita cursors
+
+    cursors.enable = false; # using adwaita cursors
     chromium.enable = false; # chromium is configured outside of home-manager
-    firefox.enable = true;
-    fish.enable = true;
-    fzf.enable = true;
+    gtk.icon.enable = false; # using adwaita icons
     hyprland.enable = false; # TODO: configure
     hyprlock.enable = false; # TODO: configure
-    k9s.enable = true;
-    kitty.enable = true;
-    kvantum.enable = true; # qt
-    mpv.enable = true;
-    nushell.enable = true;
-    nvim.enable = true;
-    rofi.enable = true;
-    spotify-player.enable = true;
-    yazi.enable = true;
-    zathura.enable = true;
+
   };
 
 }
