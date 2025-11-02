b7fab747f7617e84a02f40db0065394048c9a92e
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-11-02 16:26:19 +0100

    chore(host/zefir): removed redundant syncthing nixosModule option

--- hosts/zefir/default.nix
+++ hosts/zefir/default.nix
@@ -120,7 +120,6 @@
         devices = [ "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:6:1.0-event-mouse" ];
       };
       logrotate-nvim.enable = true;
-      # syncthing.enable = true;
       yubikey = {
         enable = true;
         yubikey-touch-detector = true;
