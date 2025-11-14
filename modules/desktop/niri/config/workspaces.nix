cbd7b2aa1e92ea48d2eb0b781c620d5de1b5308b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-11-14 16:35:58 +0100

    feat(niri/workspaces): configured workspaces per output

--- modules/desktop/niri/config/workspaces.nix
+++ modules/desktop/niri/config/workspaces.nix
@@ -1,10 +1,22 @@
 {
 
   programs.niri.settings.workspaces = {
-    "left" = { };
-    "center" = { };
-    "right" = { };
-    "special" = { };
+    "01-special" = {
+      name = "special";
+      open-on-output = "eDP-1";
+    };
+    "02-center" = {
+      name = "center";
+      open-on-output = "eDP-1";
+    };
+    "03-left" = {
+      name = "left";
+      open-on-output = "Dell Inc. DELL P2422H 8WRR0V3";
+    };
+    "04-right" = {
+      name = "right";
+      open-on-output = "Lenovo Group Limited M14t V309WMZ3";
+    };
   };
 
 }
