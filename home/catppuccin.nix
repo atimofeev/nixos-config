48a501560bce1188c867f5f87f2329023d86522b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-11-28 13:32:20 +0100

    feat(catppuccin/mpv): set background-color to black via override

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -1,4 +1,10 @@
-{ inputs, osConfig, ... }:
+{
+  config,
+  inputs,
+  osConfig,
+  pkgs,
+  ...
+}:
 {
 
   imports = [ inputs.catppuccin.homeModules.catppuccin ];
@@ -16,6 +22,17 @@
     hyprland.enable = false; # TODO: configure
     hyprlock.enable = false; # not using colors
     spotify-player.enable = false; # default theme looks better in themed terminal
+
+    # NOTE: override mpv backgroud
+    # https://github.com/catppuccin/nix/issues/468
+    sources.mpv =
+      inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.mpv.overrideAttrs
+        (oldAttrs: {
+          postPatch = (oldAttrs.postPach or "") + ''
+            substituteInPlace "themes/${config.catppuccin.mpv.flavor}/${config.catppuccin.mpv.accent}.conf" \
+              --replace-fail "background-color='#1e1e2e'" "background-color='#000000'"
+          '';
+        });
 
   };
 
