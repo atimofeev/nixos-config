d11027503cedca109e2436787ebf659f374b2d20
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-11-30 19:55:24 +0100

    chore(gtk): formatted and added comment

--- modules/desktop/hyprland/config/apps/gtk.nix
+++ modules/desktop/hyprland/config/apps/gtk.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
 {
 
-  home.packages = with pkgs; [
-    adwaita-icon-theme-legacy
-  ];
+  home = {
+
+    packages = with pkgs; [
+      adwaita-icon-theme-legacy
+    ];
+
+    sessionVariables = {
+      # NOTE: redundant? we're using the gtk portal
+      ADW_DISABLE_PORTAL = "1";
+    };
+
+  };
 
   gtk = {
     enable = true;
@@ -12,10 +21,6 @@
       package = pkgs.adwaita-icon-theme;
       name = "Adwaita";
     };
-  };
-
-  home.sessionVariables = {
-    ADW_DISABLE_PORTAL = "1";
   };
 
 }
