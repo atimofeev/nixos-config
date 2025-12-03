e3f2513da86c8d40a641e5877f73656304aaf60b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-12-03 02:13:38 +0100

    feat(modules/home): implemented automatic .nix import
    for #47

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,11 +1,43 @@
+{ lib, ... }:
 {
 
-  imports = [
+  imports =
+    lib.concatMap
+      (
+        dir:
+        lib.pipe (builtins.readDir dir) [
+          (lib.filterAttrs (path: _kind: !lib.hasPrefix "_" path))
+          (lib.filterAttrs (
+            _path: kind: kind == "directory" || (kind == "regular" && lib.hasSuffix ".nix" _path)
+          ))
+          (lib.mapAttrsToList (path: _kind: lib.path.append dir path))
+        ]
+      )
+      [
+        ./services
+      ];
 
-    ./services/cliphist.nix
-    ./services/polkit-gnome.nix
-    ./services/syncthing.nix
-
-  ];
+  # imports = [
+  #
+  #   # ./system/catppuccin.nix
+  #   # ./system/nix-index.nix
+  #   # ./system/sops.nix
+  #
+  #   ./services/cliphist.nix
+  #   ./services/polkit-gnome.nix
+  #   ./services/syncthing.nix
+  #
+  #   # ./apps/firefox
+  #   # ./apps/terminal
+  #   # ./apps/mpv.nix
+  #   # ./apps/obsidian.nix
+  #   # ./apps/qbittorrent.nix
+  #   # ./apps/slack.nix
+  #   # ./apps/swappy.nix
+  #   # ./apps/vcv-rack.nix
+  #   # ./apps/zathura.nix
+  #   # ./apps/zen-browser.nix
+  #
+  # ];
 
 }
