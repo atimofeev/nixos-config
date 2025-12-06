27c99f95ac555521ade2e3529712f6b28d34bfbe
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-12-06 19:16:40 +0100

    chore(overlays): retire neovim-unwrapped overlay with custom Exec

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
 {
 
   nixpkgs.overlays = [
-    (import ./neovim-unwrapped.nix)
     (import ./unstable.nix { inherit inputs; })
     (import ./vcv-rack.nix) # NOTE: https://github.com/NixOS/nixpkgs/issues/393113
   ];
