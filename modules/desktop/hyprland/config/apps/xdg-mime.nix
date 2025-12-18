dcfce959ab90ac4f80783a8adb18c4a5d55263a2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-12-18 09:51:20 +0100

    feat(xdg-mime): updated app associations

--- modules/desktop/hyprland/config/apps/xdg-mime.nix
+++ modules/desktop/hyprland/config/apps/xdg-mime.nix
@@ -1,4 +1,3 @@
-_:
 # NOTE: finish config
 # example: https://github.com/gytis-ivaskevicius/nixfiles/blob/f644edf7abbc5c8190421b88408a23ce73f53a4b/home-manager/common.nix#L48
 let
@@ -9,44 +8,49 @@ let
   # https://www.youtube.com/watch?v=GYW9i_u5PYs
   pdfViewer = [ "org.pwmt.zathura.desktop" ];
   tg = [ "org.telegram.desktop.desktop" ];
-  associations = {
-    "text/x-dbus-service" = editor;
-    "x-scheme-handler/tg" = tg;
-    "image/jpeg" = imageViewer;
-    "image/png" = imageViewer;
-    "image/gif" = imageViewer;
-    "image/webp" = imageViewer;
-    "image/tiff" = imageViewer;
-    "image/x-tga" = imageViewer;
-    "image/vnd-ms.dds" = imageViewer;
-    "image/x-dds" = imageViewer;
-    "image/bmp" = imageViewer;
-    "image/vnd.microsoft.icon" = imageViewer;
-    "image/vnd.radiance" = imageViewer;
-    "image/x-exr" = imageViewer;
-    "image/x-portable-bitmap" = imageViewer;
-    "image/x-portable-graymap" = imageViewer;
-    "image/x-portable-pixmap" = imageViewer;
-    "image/x-portable-anymap" = imageViewer;
-    "image/x-qoi" = imageViewer;
-    "image/svg+xml" = imageViewer;
-    "image/svg+xml-compressed" = imageViewer;
-    "image/avif" = imageViewer;
-    "image/heic" = imageViewer;
-    "image/jxl" = imageViewer;
+  add_associations = {
     "application/pdf" = pdfViewer;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/chrome" = browser;
-    "text/html" = browser;
     "application/x-extension-htm" = browser;
     "application/x-extension-html" = browser;
     "application/x-extension-shtml" = browser;
-    "application/xhtml+xml" = browser;
-    "application/x-extension-xhtml" = browser;
     "application/x-extension-xht" = browser;
-    # TODO: fix
-    # "inode/directory" = fileBrowser;
+    "application/x-extension-xhtml" = browser;
+    "application/xhtml+xml" = browser;
+    "image/avif" = imageViewer;
+    "image/bmp" = imageViewer;
+    "image/gif" = imageViewer;
+    "image/heic" = imageViewer;
+    "image/jpeg" = imageViewer;
+    "image/jxl" = imageViewer;
+    "image/png" = imageViewer;
+    "image/svg+xml" = imageViewer;
+    "image/svg+xml-compressed" = imageViewer;
+    "image/tiff" = imageViewer;
+    "image/vnd-ms.dds" = imageViewer;
+    "image/vnd.microsoft.icon" = imageViewer;
+    "image/vnd.radiance" = imageViewer;
+    "image/webp" = imageViewer;
+    "image/x-dds" = imageViewer;
+    "image/x-exr" = imageViewer;
+    "image/x-portable-anymap" = imageViewer;
+    "image/x-portable-bitmap" = imageViewer;
+    "image/x-portable-graymap" = imageViewer;
+    "image/x-portable-pixmap" = imageViewer;
+    "image/x-qoi" = imageViewer;
+    "image/x-tga" = imageViewer;
+    "inode/directory" = fileBrowser;
+    "text/html" = browser;
+    "text/x-dbus-service" = editor;
+    "x-scheme-handler/chrome" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/tg" = tg;
+  };
+  remove_associations = {
+    "inode/directory" = [
+      "kitty-open.desktop"
+      "org.pwmt.zathura-cb.desktop"
+    ];
   };
 in
 {
@@ -54,8 +58,11 @@ in
     configFile."mimeapps.list".force = true;
     mimeApps = {
       enable = true;
-      defaultApplications = associations;
-      associations.added = associations;
+      defaultApplications = add_associations;
+      associations = {
+        added = add_associations;
+        removed = remove_associations;
+      };
     };
   };
 }
