62fc9f400b15a2aa1c76923bb628448da402eab8
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2025-12-21 23:12:08 +0100

    feat(host/zefir): tlp - disabled cpu turbo boost to prevent throttling

--- hosts/zefir/default.nix
+++ hosts/zefir/default.nix
@@ -129,7 +129,12 @@
         devices = [ "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:6:1.0-event-mouse" ];
       };
       logrotate-nvim.enable = true;
-      tlp.enable = true;
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_BOOST_ON_AC = 0;
+        };
+      };
       yubikey = {
         enable = true;
         yubikey-touch-detector = true;
