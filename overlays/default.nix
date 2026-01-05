ba13994a485fab7bfc70c90d2816a89aa79337a7
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-01-05 23:21:41 +0100

    fix(xow_dongle-firmware): xone dongle firmware files couldn't be found

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,8 @@
 
   nixpkgs.overlays = [
     (import ./unstable.nix { inherit inputs; })
-    (import ./vcv-rack.nix) # NOTE: https://github.com/NixOS/nixpkgs/issues/393113
+    (import ./vcv-rack.nix) # BUG: https://github.com/NixOS/nixpkgs/issues/393113
+    (import ./xow_dongle-firmware.nix) # BUG: https://github.com/NixOS/nixpkgs/issues/471331
   ];
 
 }
