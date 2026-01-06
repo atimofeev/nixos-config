ba58e42e0ace95180d16fd86af5a95d936c35df2
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-01-06 22:03:50 +0100

    feat(vicinae): added catppuccin extension

--- modules/home/services/vicinae.nix
+++ modules/home/services/vicinae.nix
@@ -44,6 +44,11 @@ in
         #   rev = "b8c8fcd7ebd441a5452b396923f2a40e879565ba";
         # })
         # (config.lib.vicinae.mkRayCastExtension {
+        #   name = "catppuccin";
+        #   sha256 = "sha256-mR1jpp1PAVae2P5PB+cr4jbYF47EEtdj6koKRRnU7wo=";
+        #   rev = "f8ee7250696ef4ba4b7f20804bb3ea99dff24aab";
+        # })
+        # (config.lib.vicinae.mkRayCastExtension {
         #   name = "weather";
         #   sha256 = "sha256-GopBV/7AiOTUYdz3anWcL2gd9dDg+6tgO4wckO4ATRs=";
         #   rev = "542ed079c2eb5a95df0835d83ab1f1c2b1970e44";
