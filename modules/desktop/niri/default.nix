d97fb1a71a12b96f8341373c747d2bad742fe946
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-01-29 09:58:03 +0100

    chore(niri): cleaned up and formatted

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -1,26 +1,24 @@
 {
-  inputs,
   pkgs,
   vars,
   ...
 }:
 {
 
-  # imports = [ inputs.niri.nixosModules.niri ];
-
   home-manager.users.${vars.username} = import ./config;
 
   environment.systemPackages = [ pkgs.unstable.xwayland-satellite ];
 
-  programs.niri = {
-    enable = true;
-    package = pkgs.niri;
-  };
+  programs = {
 
-  programs.uwsm.waylandCompositors.niri = {
-    prettyName = "Niri";
-    comment = "A scrollable-tiling Wayland compositor";
-    binPath = "/run/current-system/sw/bin/niri-session";
+    niri.enable = true;
+
+    uwsm.waylandCompositors.niri = {
+      prettyName = "Niri";
+      comment = "A scrollable-tiling Wayland compositor";
+      binPath = "/run/current-system/sw/bin/niri-session";
+    };
+
   };
 
 }
