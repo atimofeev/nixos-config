44dd331b7a75326819038f4cf6c2a2fe4434ba34
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-02-09 20:53:44 +0100

    feat(zen-browser): removed github pin

--- modules/home/apps/zen-browser.nix
+++ modules/home/apps/zen-browser.nix
@@ -63,14 +63,6 @@ in
 
         pinsForce = true;
         pins = {
-          "GitHub" = {
-            id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
-            workspace = spaces."Research".id;
-            url = "https://github.com";
-            position = 101;
-            isEssential = false;
-            editedTitle = true;
-          };
           "Gmail" = {
             id = "1eabb6a3-911b-4fa9-9eaf-232a3703db19";
             workspace = spaces."Work".id;
