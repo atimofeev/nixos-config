3ffc0f3a13dbc796866d55cc355d420d1b72a1e8
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-03-18 23:32:14 +0100

    feat(system/secrets): added personal/env-vars sourcing

--- modules/system/secrets.nix
+++ modules/system/secrets.nix
@@ -11,6 +11,11 @@ in
   config = lib.mkIf cfg.enable {
 
     environment.shellInit = ''
+      if [ -r ${config.sops.secrets."personal/env-vars".path} ]; then
+        set -a
+        source ${config.sops.secrets."personal/env-vars".path}
+        set +a
+      fi
       if [ -r ${config.sops.secrets."work/env-vars".path} ]; then
         set -a
         source ${config.sops.secrets."work/env-vars".path}
@@ -29,6 +34,7 @@ in
     };
 
     sops.secrets = {
+      "personal/env-vars".owner = config.custom.hm-admin;
       "work/env-vars".owner = config.custom.hm-admin;
       "work/homepage-env" = {
         owner = config.custom.hm-admin;
