c1a593f50e395a42fe2d2b485bbe6bc83ab0b849
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-03-22 10:28:33 +0100

    feat(niri): reworked workspaces configuration

--- modules/desktop/niri/config/workspaces.nix
+++ modules/desktop/niri/config/workspaces.nix
@@ -5,17 +5,17 @@
       name = "special";
       open-on-output = "eDP-1";
     };
-    "02-center" = {
-      name = "center";
-      open-on-output = "eDP-1";
-    };
-    "03-left" = {
-      name = "left";
-      open-on-output = "Dell Inc. DELL P2422H 8WRR0V3";
-    };
-    "04-right" = {
+    "02-right" = {
       name = "right";
       open-on-output = "Lenovo Group Limited M14t V309WMZ3";
+    };
+    "03-right-work" = {
+      name = "right-work";
+      open-on-output = "Dell Inc. DELL P2422H 4X6V7N3";
+    };
+    "04-left-work" = {
+      name = "left-work";
+      open-on-output = "Dell Inc. DELL P2422H 8WRR0V3";
     };
   };
 
