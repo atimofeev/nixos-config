8eda08b7241a6cc5d19d0ddea4d7feca78252582
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-04-22 15:10:44 +0200

    chore(niri/outputs): removed mode hardcode for outputs

--- modules/desktop/niri/config/outputs.nix
+++ modules/desktop/niri/config/outputs.nix
@@ -3,11 +3,6 @@
   programs.niri.settings.outputs = {
 
     "Samsung Display Corp. ATNA60DL01-0  Unknown" = {
-      mode = {
-        width = 2560;
-        height = 1600;
-        refresh = 240.0;
-      };
       position = {
         x = 0;
         y = 0;
@@ -17,11 +12,6 @@
     };
 
     "BOE 0x0747" = {
-      mode = {
-        width = 1920;
-        height = 1080;
-        refresh = 60.0;
-      };
       position = {
         x = 0;
         y = 0;
@@ -30,20 +20,10 @@
     };
 
     "Lenovo Group Limited M14t V309WMZ3" = {
-      mode = {
-        width = 1920;
-        height = 1080;
-        refresh = 60.0;
-      };
       scale = 1.2;
     };
 
     "Dell Inc. DELL P2422H 8WRR0V3" = {
-      mode = {
-        width = 1920;
-        height = 1080;
-        refresh = 60.0;
-      };
       position = {
         x = -1920;
         y = 0;
@@ -52,11 +32,6 @@
     };
 
     "Dell Inc. DELL P2422H 4X6V7N3" = {
-      mode = {
-        width = 1920;
-        height = 1080;
-        refresh = 60.0;
-      };
       scale = 1;
     };
 
