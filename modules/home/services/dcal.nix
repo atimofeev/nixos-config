309c28459824b39a078e8697add0c32c84d6f646
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-08-16 01:30:15 +0200

    feat(dcal): add ui-settings defaults to dank-calendar settings

--- modules/home/services/dcal.nix
+++ modules/home/services/dcal.nix
@@ -21,6 +21,38 @@ in
       enable = true;
       systemd.enable = true;
       quickshell.package = pkgs.unstable.quickshell;
+      settings = {
+        allDayReminderDaysBefore = 0;
+        allDayReminderTime = "09:00";
+        allDayReminders = true;
+        closeBehavior = "minimize";
+        colorSource = "auto";
+        coreHoursEnabled = true;
+        coreHoursEnd = 22;
+        coreHoursStart = 8;
+        customThemeFile = "";
+        defaultEventDurationMinutes = 30;
+        defaultReminderMinutes = 10;
+        firstDayOfWeek = -1;
+        lastView = "agenda";
+        monthEventTitleLines = 1;
+        monthShowAllEvents = false;
+        notificationSounds = false;
+        presetTheme = "purple";
+        reminderPersist = true;
+        remindersEnabled = true;
+        showTasks = true;
+        showTrayIcon = false;
+        showWeekNumbers = true;
+        sidebarCollapsed = false;
+        sidebarWidth = 240;
+        snoozeMinutes = 5;
+        syncIntervalMinutes = 15;
+        themeMode = "auto";
+        timeFormat = "auto";
+        use24HourClock = true;
+        weekEventTitleLines = 1;
+      };
     };
   };
 
