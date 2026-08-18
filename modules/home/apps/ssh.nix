8b00e0e0c93c02c44b283f2caa16d33cc70ade9c
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-08-18 16:00:21 +0200

    feat(ssh): restart ssh-agent on failure and refactor ssh-add service

--- modules/home/apps/ssh.nix
+++ modules/home/apps/ssh.nix
@@ -31,21 +31,25 @@ in
 
     services.ssh-agent.enable = true;
 
-    systemd.user.services.ssh-add = {
-      Unit = {
-        Description = "Add SSH keys to the agent";
-        After = "ssh-agent.service";
-        PartOf = "graphical-session.target";
+    systemd.user.services = {
+      ssh-add = {
+        Unit = {
+          Description = "Add SSH keys to the agent";
+          After = "ssh-agent.service";
+          PartOf = "graphical-session.target";
+        };
+        Install.WantedBy = [ "graphical-session.target" ];
+        Service = {
+          Type = "oneshot";
+          ExecStart = lib.getExe' pkgs.openssh "ssh-add";
+          RemainAfterExit = false;
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
       };
-      Install.WantedBy = [ "graphical-session.target" ];
-      Service = {
-        Type = "oneshot";
-        ExecStart = lib.getExe' pkgs.openssh "ssh-add";
-        RemainAfterExit = false;
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
+
+      ssh-agent.Service.Restart = "on-failure";
     };
 
   };
