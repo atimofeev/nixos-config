fb55bf8b495c1e17bbd05b979bdccb846024322d
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-08-24 09:43:53 +0200

    chore(niri): disable direct scanout debug flag

--- users/atimofeev/niri/debug.nix
+++ users/atimofeev/niri/debug.nix
@@ -5,6 +5,7 @@
       honor-xdg-activation-with-invalid-serial = { }; # NOTE: fix for callbacks & window activation
       # NOTE: fix for iGPU vs dGPU performance issues when multiple monitors are connected
       disable-cursor-plane = { };
+      disable-direct-scanout = { };
       # enable-overlay-planes = { };
       # wait-for-frame-completion-before-queueing = { };
     };
