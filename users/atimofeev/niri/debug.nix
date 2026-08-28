6be4dedf921a01047f811ce74c52bccb35709a1b
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-08-28 19:20:01 +0200

    fix(niri): swap cursor-plane and frame-completion workarounds

--- users/atimofeev/niri/debug.nix
+++ users/atimofeev/niri/debug.nix
@@ -4,10 +4,10 @@
     debug = {
       honor-xdg-activation-with-invalid-serial = { }; # NOTE: fix for callbacks & window activation
       # NOTE: fix for iGPU vs dGPU performance issues when multiple monitors are connected
-      disable-cursor-plane = { };
+      # disable-cursor-plane = { };
       disable-direct-scanout = { };
       # enable-overlay-planes = { };
-      # wait-for-frame-completion-before-queueing = { };
+      wait-for-frame-completion-before-queueing = { };
     };
   };
 
