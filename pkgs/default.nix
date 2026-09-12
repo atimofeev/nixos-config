ef1e733498cf9dcdde7d7d16d17bcb3117a222ff
Artem Timofeev <39891735+atimofeev@users.noreply.github.com>
2026-09-12 21:11:20 +0200

    chore(pkgs): removed stale definitions

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ ... }:
 
 {
 
@@ -7,13 +7,9 @@
       awsx = prev.callPackage ./awsx.nix { };
       # falcon = prev.callPackage ./falcon.nix { };
       pi-codex-conversion-helpers = prev.callPackage ./pi-codex-conversion-helpers.nix { };
-      darkreader-declarative =
-        inputs.firefox-extensions-declarative.packages.${prev.stdenv.hostPlatform.system}.darkreader-declarative;
       kubectl-login = prev.callPackage ./kubectl-login.nix { };
       linux-g14 = prev.callPackage ./linux-g14.nix { };
       nvidia-hide = prev.callPackage ./nvidia-hide.nix { };
-      surfingkeys-declarative =
-        inputs.firefox-extensions-declarative.packages.${prev.stdenv.hostPlatform.system}.surfingkeys-declarative;
       glci = prev.callPackage ./glci.nix { };
       vault-kv-mv = prev.callPackage ./vault-kv-mv.nix { };
     })
